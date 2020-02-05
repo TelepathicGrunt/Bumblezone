4299063794479f41dcfdf6b86464f8263365b02d
alex sarnese <alexthepokemon@Gmail.com>
2020-02-05 17:23:06 -0500

    added teleporting out of bumblezone

--- src/main/java/net/telepathicgrunt/bumblezone/teleportation/Teleportation.java
+++ src/main/java/net/telepathicgrunt/bumblezone/teleportation/Teleportation.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.item.EnderPearlEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.Vec3d;
@@ -23,6 +24,7 @@ import net.telepathicgrunt.bumblezone.Bumblezone;
 import net.telepathicgrunt.bumblezone.capabilities.IPlayerPosAndDim;
 import net.telepathicgrunt.bumblezone.capabilities.PlayerPositionAndDimension;
 import net.telepathicgrunt.bumblezone.world.dimension.BumblezoneDimension;
+import net.telepathicgrunt.bumblezone.world.feature.placement.PlacingUtils;
 
 
 @Mod.EventBusSubscriber(modid = Bumblezone.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
@@ -130,67 +132,217 @@ public class Teleportation
 			PlayerEntity playerEntity = event.player;
 			PlayerPositionAndDimension cap = (PlayerPositionAndDimension) playerEntity.getCapability(PAST_POS_AND_DIM).orElseThrow(RuntimeException::new);
 			
-			
-			if (cap.isTeleporting && !playerEntity.world.isRemote && playerEntity.world instanceof ServerWorld)
+			if(!playerEntity.world.isRemote && playerEntity.world instanceof ServerWorld)
 			{
-
-				//gets the world in the destination dimension
-				MinecraftServer minecraftServer = playerEntity.getServer(); // the server itself
-				ServerWorld serverWorld = minecraftServer.getWorld(cap.nextDimension); // gets the new destination dimension 
-				
-				//scales the position to get the corresponding position in bumblezone dim
-				BlockPos blockpos = new BlockPos(
-						playerEntity.getPosition().getX() * serverWorld.getDimension().getMovementFactor(), 
-						playerEntity.getPosition().getY(), 
-						playerEntity.getPosition().getZ() * serverWorld.getDimension().getMovementFactor());
-				
-				
-				//gets valid space in other world
-				BlockPos validBlockPos = validPlayerSpawnLocation(serverWorld, blockpos, 16);
-				
-				
-				if (validBlockPos == null)
+				//teleported by pearl outside bumblezone dimension
+				if (cap.isTeleporting)
 				{
-					//No valid space found around portal. 
-					//We are going to spawn player at exact spot of scaled coordinates by placing air at the spot
-					serverWorld.setBlockState(blockpos, Blocks.AIR.getDefaultState());
-					serverWorld.setBlockState(blockpos.up(), Blocks.AIR.getDefaultState());
-					validBlockPos = blockpos;
+					teleportByPearl(playerEntity, cap);
 				}
-
-
-				//if player throws pearl at hive and then goes to sleep, they wake up
-				if (playerEntity.isSleeping())
+				//teleported by going out of bounds inside bumblezone dimension
+				else if(playerEntity.dimension == BumblezoneDimension.bumblezone() && 
+					     (playerEntity.getY() < 0 || playerEntity.getY() > 255)) 
 				{
-					playerEntity.wakeUp();
+					teleportByOutOfBounds(playerEntity, cap, playerEntity.getY() < 0 ? true : false);
 				}
-
-
-				//let game know we are gonna teleport player
-				ChunkPos chunkpos = new ChunkPos(validBlockPos);
-				serverWorld.getChunkProvider().registerTicket(TicketType.POST_TELEPORT, chunkpos, 1, playerEntity.getEntityId());
-				
-				((ServerPlayerEntity)playerEntity).teleport(
-					serverWorld, 
-					validBlockPos.getX() + 0.5D, 
-					validBlockPos.getY() + 1, 
-					validBlockPos.getZ() + 0.5D, 
-					playerEntity.rotationYaw, 
-					playerEntity.rotationPitch);
-				
-				//teleportation complete. 
-				cap.setTeleporting(false);
 			}
 		}
 	}
+	
+	
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//Teleporting
+	
+	
+	private static void teleportByOutOfBounds(PlayerEntity playerEntity, PlayerPositionAndDimension cap, boolean checkingUpward)
+	{
+		//gets the world in the destination dimension
+		MinecraftServer minecraftServer = playerEntity.getServer(); // the server itself
+		ServerWorld destinationWorld;
+		
+		//Error. This shouldn't be. We aren't leaving the bumblezone to go to the bumblezone. 
+		//Go to Overworld instead as default
+		if(cap.prevDimension == BumblezoneDimension.bumblezone())
+		{
+			destinationWorld = minecraftServer.getWorld(DimensionType.OVERWORLD); // go to overworld by default
+		}
+		else 
+		{
+			destinationWorld = minecraftServer.getWorld(cap.prevDimension); // gets the previous dimension user came from
+		}
+		
+		
+		//scales the position to get the corresponding position in non-bumblezone dimension
+		BlockPos blockpos = new BlockPos(
+				playerEntity.getPosition().getX() / destinationWorld.getDimension().getMovementFactor(), 
+				playerEntity.getPosition().getY(), 
+				playerEntity.getPosition().getZ() / destinationWorld.getDimension().getMovementFactor());
+		
+		
+		//Gets valid space in other world
+		//Won't ever be null
+		BlockPos validBlockPos = validPlayerSpawnLocationByBeehive(destinationWorld, blockpos, 16, checkingUpward);
+		
 
+		//if player throws pearl at hive and then goes to sleep, they wake up
+		if (playerEntity.isSleeping())
+		{
+			playerEntity.wakeUp();
+		}
+
+
+		//let game know we are gonna teleport player
+		ChunkPos chunkpos = new ChunkPos(validBlockPos);
+		destinationWorld.getChunkProvider().registerTicket(TicketType.POST_TELEPORT, chunkpos, 1, playerEntity.getEntityId());
+		
+		((ServerPlayerEntity)playerEntity).teleport(
+			destinationWorld, 
+			validBlockPos.getX() + 0.5D, 
+			validBlockPos.getY() + 1, 
+			validBlockPos.getZ() + 0.5D, 
+			playerEntity.rotationYaw, 
+			playerEntity.rotationPitch);
+		
+		//teleportation complete. 
+		cap.setTeleporting(false);
+	}
+	
+	private static void teleportByPearl(PlayerEntity playerEntity, PlayerPositionAndDimension cap)
+	{
+		//gets the world in the destination dimension
+		MinecraftServer minecraftServer = playerEntity.getServer(); // the server itself
+		ServerWorld destinationWorld = minecraftServer.getWorld(cap.nextDimension); // gets the new destination dimension 
+
+		
+		//scales the position to get the corresponding position in bumblezone dimension
+		BlockPos blockpos = new BlockPos(
+				playerEntity.getPosition().getX() * destinationWorld.getDimension().getMovementFactor(), 
+				playerEntity.getPosition().getY(), 
+				playerEntity.getPosition().getZ() * destinationWorld.getDimension().getMovementFactor());
+		
+		
+		//gets valid space in other world
+		BlockPos validBlockPos = validPlayerSpawnLocation(destinationWorld, blockpos, 16);
+		
+		
+		if (validBlockPos == null)
+		{
+			//No valid space found around portal. 
+			//We are going to spawn player at exact spot of scaled coordinates by placing air at the spot
+			destinationWorld.setBlockState(blockpos, Blocks.AIR.getDefaultState());
+			destinationWorld.setBlockState(blockpos.up(), Blocks.AIR.getDefaultState());
+			validBlockPos = blockpos;
+		}
+
+
+		//if player throws pearl at hive and then goes to sleep, they wake up
+		if (playerEntity.isSleeping())
+		{
+			playerEntity.wakeUp();
+		}
+
+
+		//let game know we are gonna teleport player
+		ChunkPos chunkpos = new ChunkPos(validBlockPos);
+		destinationWorld.getChunkProvider().registerTicket(TicketType.POST_TELEPORT, chunkpos, 1, playerEntity.getEntityId());
+		
+		((ServerPlayerEntity)playerEntity).teleport(
+			destinationWorld, 
+			validBlockPos.getX() + 0.5D, 
+			validBlockPos.getY() + 1, 
+			validBlockPos.getZ() + 0.5D, 
+			playerEntity.rotationYaw, 
+			playerEntity.rotationPitch);
+		
+		//teleportation complete. 
+		cap.setTeleporting(false);
+	}
+
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	//Util
+	
+	
+	private static BlockPos validPlayerSpawnLocationByBeehive(World world, BlockPos position, int maximumRange, boolean checkingUpward)
+	{
+		int radius = 16 * 16 - 1;
+		
+		//snaps the coordinates to chunk origin and then sets height to minimum or maximum based on search direction
+		BlockPos.Mutable mutableBlockPos = new BlockPos.Mutable(
+														position.getX(), 
+														checkingUpward ? 0 : world.getDimension().getActualHeight(), 
+														position.getZ()); 
+		
+		
+		for (; mutableBlockPos.getY() < 0 || mutableBlockPos.getY() > world.getDimension().getActualHeight();)
+		{
+			for (int range = 1; range < maximumRange; range++)
+			{
+				for (int x = -range; x <= range; x++)
+				{
+					for (int z = -range; z <= range; z++)
+					{
+						if (x * x + z * z >= radius)
+						{
+							mutableBlockPos.setPos(position.getX() + x, mutableBlockPos.getY(), position.getZ() + z);
+							
+							if (world.getBlockState(mutableBlockPos).getBlock() == Blocks.field_226905_ma_)
+							{
+								//A Hive was found, try to find a valid spot next to it
+								BlockPos validSpot = validPlayerSpawnLocation(world, mutableBlockPos, 3);
+								if(validSpot != null) {
+									return validSpot;
+								}
+							}
+						}
+					}
+				}
+			}
+			
+			//move the block pos
+			if(checkingUpward)
+			{
+				mutableBlockPos.move(Direction.UP);
+			}
+			else
+			{
+				mutableBlockPos.move(Direction.DOWN);
+			}
+		}
+		
+		
+		//no valid spot was found, generate a hive and spawn us on the highest land
+		mutableBlockPos.setPos(
+						position.getX(), 
+						PlacingUtils.topOfSurfaceBelowHeight(world, world.getDimension().getActualHeight(), 0, world.rand, position), 
+						position.getZ());
+		
+		if(mutableBlockPos.getY() > 0)
+		{
+			world.setBlockState(mutableBlockPos.up(), Blocks.AIR.getDefaultState());
+			return mutableBlockPos;
+		}
+		else
+		{
+			//No valid spot was found. Just place character on a generate hive at center of height of coordinate 
+			//Basically just f*** it at this point lol
+			mutableBlockPos.setPos(
+							position.getX(), 
+							world.getDimension().getActualHeight()/2, 
+							position.getZ());
+
+			world.setBlockState(mutableBlockPos.up(), Blocks.AIR.getDefaultState());
+			return mutableBlockPos;
+		}
+	}
+	
 	@Nullable
 	private static BlockPos validPlayerSpawnLocation(World world, BlockPos position, int maximumRange)
 	{
 		//Try to find 2 non-solid spaces around it that the player can spawn at
 		int radius = 0;
 
-		for (int range = 1; range < maximumRange; range++)
+		for (int range = 0; range < maximumRange; range++)
 		{
 			radius = range * range * range - 1;
 
