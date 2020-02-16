455badee1dbc8705050c2f90c89ee1ff695da372
alex sarnese <alexthepokemon@Gmail.com>
2020-02-16 16:54:32 -0500

    getting started

--- src/main/java/net/telepathicgrunt/bumblezone/Bumblezone.java
+++ src/main/java/net/telepathicgrunt/bumblezone/Bumblezone.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package net.telepathicgrunt.bumblezone;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class Bumblezone implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
