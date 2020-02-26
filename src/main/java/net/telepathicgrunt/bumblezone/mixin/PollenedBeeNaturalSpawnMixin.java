c48fc135900aadbb65aa34ccfba863f3754f2865
alex sarnese <alexthepokemon@Gmail.com>
2020-02-25 20:54:41 -0500

    fixed crash AGAIN

--- src/main/java/net/telepathicgrunt/bumblezone/mixin/PollenedBeeNaturalSpawnMixin.java
+++ src/main/java/net/telepathicgrunt/bumblezone/mixin/PollenedBeeNaturalSpawnMixin.java
@@ -19,8 +19,7 @@ public class PollenedBeeNaturalSpawnMixin
 {
     //spawns bees with chance to bee full of pollen or be a BeeProductive mob if that mod is on
     @ModifyArg(method = "spawnEntitiesInChunk",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;spawnEntity(Lnet/minecraft/entity/Entity;)Z"),
-            index = 0)
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;spawnEntity(Lnet/minecraft/entity/Entity;)Z"))
     private static Entity spawnEntitiesInChunk(Entity entity) {
         if(!(entity.world.isClient()))
         {
