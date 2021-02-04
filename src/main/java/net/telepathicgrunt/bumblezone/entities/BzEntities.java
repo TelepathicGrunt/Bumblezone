da32b8449b04eed5f8ae75ef9620209572b3338b
Alex Sarnese <40846040+TelepathicGrunt@users.noreply.github.com>
2021-02-04 18:47:55 -0500

    turn off logspam for datafixers

--- src/main/java/net/telepathicgrunt/bumblezone/entities/BzEntities.java
+++ src/main/java/net/telepathicgrunt/bumblezone/entities/BzEntities.java
@@ -1,6 +1,7 @@
 package net.telepathicgrunt.bumblezone.entities;
 
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.SharedConstants;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.SpawnRestriction;
@@ -14,7 +15,14 @@ import net.telepathicgrunt.bumblezone.mixin.entities.SpawnRestrictionAccessor;
 
 public class BzEntities {
 
-    public static final EntityType<HoneySlimeEntity> HONEY_SLIME = EntityType.Builder.<HoneySlimeEntity>create(HoneySlimeEntity::new, SpawnGroup.CREATURE).setDimensions(0.6F, 1.99F).maxTrackingRange(8).build("honey_slime");
+    public static final EntityType<HoneySlimeEntity> HONEY_SLIME;
+    static {
+        // turns off "No data fixer registered for honey_slime" log spam just for our entity.
+        boolean datafixer = SharedConstants.useChoiceTypeRegistrations;
+        SharedConstants.useChoiceTypeRegistrations = false;
+        HONEY_SLIME = EntityType.Builder.<HoneySlimeEntity>create(HoneySlimeEntity::new, SpawnGroup.CREATURE).setDimensions(0.6F, 1.99F).maxTrackingRange(8).build("honey_slime");
+        SharedConstants.useChoiceTypeRegistrations = datafixer;
+    }
 
     public static void registerEntities() {
         Registry.register(Registry.ENTITY_TYPE, new Identifier(Bumblezone.MODID, "honey_slime"), HONEY_SLIME);
