5cb6bec54db43f5bfda63fe4a3ec03d98cda49a5
TelepathicGrunt <alexsarnese@gmail.com>
2021-09-03 12:22:04 -0400

    fixed block overlay

--- src/main/java/com/telepathicgrunt/the_bumblezone/client/rendering/PileOfPollenRenderer.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/client/rendering/PileOfPollenRenderer.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.telepathicgrunt.the_bumblezone.Bumblezone;
 import com.telepathicgrunt.the_bumblezone.modinit.BzBlocks;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
@@ -12,9 +13,13 @@ import net.minecraft.client.renderer.WorldVertexBufferUploader;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.vector.Vector3f;
 import net.minecraftforge.client.event.RenderBlockOverlayEvent;
 
 
@@ -30,8 +35,34 @@ public class PileOfPollenRenderer {
         BlockState state = playerEntity.level.getBlockState(blockPos);
         if (state.is(BzBlocks.PILE_OF_POLLEN.get()))
         {
-            if(!state.getShape(playerEntity.level, blockPos).bounds().contains(playerEntity.getEyePosition(1).subtract(Vector3d.atLowerCornerOf(blockPos)))) {
+            boolean isInPollen = false;
+            for(int x = -1; x <= 1; x++) {
+                for(int z = -1; z <= 1; z++) {
+                    for(int y = -1; y <= 1; y++) {
+                        // Squared to make it positive always for the addition
+                        // Skips all non corner spots
+                        if((x*x) + (y*y) + (z*z) <= 2) continue;
+
+                        double eyePosX = playerEntity.getX() + x * playerEntity.getBbWidth() * 0.155F;
+                        double eyePosY = playerEntity.getEyeY() + y * 0.1F;
+                        double eyePosZ = playerEntity.getZ() + z * playerEntity.getBbWidth() * 0.155F;
+                        Vector3d eyePosition = new Vector3d(eyePosX, eyePosY, eyePosZ);
+                        BlockPos eyeBlockPos = new BlockPos(eyePosition);
+                        BlockState eyeBlock = playerEntity.level.getBlockState(eyeBlockPos);
+                        VoxelShape blockBounds = eyeBlock.getShape(playerEntity.level, eyeBlockPos);
+                        if (!blockBounds.isEmpty() && blockBounds.bounds().contains(eyePosition.subtract(Vector3d.atLowerCornerOf(eyeBlockPos)))) {
+                            isInPollen = true;
+                            x = 2;
+                            z = 2;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if(!isInPollen) {
                 event.setCanceled(true);
+                return;
             }
             
             Minecraft minecraftIn = Minecraft.getInstance();
@@ -39,19 +70,42 @@ public class PileOfPollenRenderer {
             BufferBuilder bufferbuilder = Tessellator.getInstance().getBuilder();
             RenderSystem.enableBlend();
             RenderSystem.defaultBlendFunc();
-            float f7 = -event.getPlayer().yRot / 64.0F;
-            float f8 = event.getPlayer().xRot / 64.0F;
-            Matrix4f matrix4f = event.getMatrixStack().last().pose();
-            bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR_TEX);
             float opacity = 1f;
             float brightness = 0.3f;
             float redStrength = 1f;
             float greenStrength = 0.9f;
             float blueStrength = 0.8f;
-            bufferbuilder.vertex(matrix4f, -1.0F, -1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity).uv(4.0F + f7, 4.0F + f8).endVertex();
-            bufferbuilder.vertex(matrix4f, 1.0F, -1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity).uv(0.0F + f7, 4.0F + f8).endVertex();
-            bufferbuilder.vertex(matrix4f, 1.0F, 1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity).uv(0.0F + f7, 0.0F + f8).endVertex();
-            bufferbuilder.vertex(matrix4f, -1.0F, 1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity).uv(4.0F + f7, 0.0F + f8).endVertex();
+
+            float pitch = -playerEntity.yRot / 64.0F;
+            float yaw = playerEntity.xRot / 64.0F;
+            float yawPlus4 = 4.0F + yaw;
+            float pitchPlus4 = 4.0F + pitch;
+
+            float movementScaling = 0.85f;
+            Vector3f playerPosition = new Vector3f(playerEntity.position().multiply(movementScaling, movementScaling, movementScaling));
+            float smallXZOffset = playerPosition.x() * playerPosition.z() * 0;
+            float smallYOffset = playerPosition.y() * 0.33f;
+
+            Matrix4f matrix4f = event.getMatrixStack().last().pose();
+            bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR_TEX);
+            bufferbuilder.vertex(matrix4f, -1.0F, -1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity).uv(pitchPlus4 - smallXZOffset, yawPlus4 - playerPosition.y()).endVertex();
+            bufferbuilder.vertex(matrix4f, 1.0F, -1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity).uv(pitch - smallXZOffset, yawPlus4 - playerPosition.y()).endVertex();
+            bufferbuilder.vertex(matrix4f, 1.0F, 1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity).uv(pitch - smallXZOffset, yaw - playerPosition.y()).endVertex();
+            bufferbuilder.vertex(matrix4f, -1.0F, 1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity).uv(pitchPlus4 - smallXZOffset, yaw - playerPosition.y()).endVertex();
+            bufferbuilder.end();
+            WorldVertexBufferUploader.end(bufferbuilder);
+            bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR_TEX);
+            bufferbuilder.vertex(matrix4f, -1.0F, -1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity * 0.33f).uv(pitchPlus4 - playerPosition.x(), yawPlus4 - smallYOffset).endVertex();
+            bufferbuilder.vertex(matrix4f, 1.0F, -1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity * 0.33f).uv(pitch - playerPosition.x(), yawPlus4 - smallYOffset).endVertex();
+            bufferbuilder.vertex(matrix4f, 1.0F, 1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity * 0.33f).uv(pitch - playerPosition.x(), yaw - smallYOffset).endVertex();
+            bufferbuilder.vertex(matrix4f, -1.0F, 1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity * 0.33f).uv(pitchPlus4 - playerPosition.x(), yaw - smallYOffset).endVertex();
+            bufferbuilder.end();
+            WorldVertexBufferUploader.end(bufferbuilder);
+            bufferbuilder.begin(7, DefaultVertexFormats.POSITION_COLOR_TEX);
+            bufferbuilder.vertex(matrix4f, -1.0F, -1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity * 0.33f).uv(pitchPlus4 - playerPosition.z(), yawPlus4 - smallYOffset).endVertex();
+            bufferbuilder.vertex(matrix4f, 1.0F, -1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity * 0.33f).uv(pitch - playerPosition.z(), yawPlus4 - smallYOffset).endVertex();
+            bufferbuilder.vertex(matrix4f, 1.0F, 1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity * 0.33f).uv(pitch - playerPosition.z(), yaw - smallYOffset).endVertex();
+            bufferbuilder.vertex(matrix4f, -1.0F, 1.0F, -0.5F).color(brightness * redStrength, brightness * greenStrength, brightness * blueStrength, opacity * 0.33f).uv(pitchPlus4 - playerPosition.z(), yaw - smallYOffset).endVertex();
             bufferbuilder.end();
             WorldVertexBufferUploader.end(bufferbuilder);
             RenderSystem.disableBlend();
