062422eb8e47b63e3d1690d404c1f5313e35c054
TelepathicGrunt <alexsarnese@gmail.com>
2021-10-02 14:49:45 -0400

    fixed bees drowning in honey

---
+++ src/main/java/com/telepathicgrunt/bumblezone/mixin/entities/BeeEntityMixin.java
@@ -0,0 +1,35 @@
+package com.telepathicgrunt.bumblezone.mixin.entities;
+
+import com.telepathicgrunt.bumblezone.tags.BzFluidTags;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.BeeEntity;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BeeEntity.class)
+public abstract class BeeEntityMixin extends Entity {
+
+    @Shadow
+    private int ticksInsideWater;
+
+    public BeeEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    /**
+     * @author TelepathicGrunt
+     * @reason make bees not drown in honey fluid
+     */
+    @Inject(method = "mobTick()V",
+            at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/entity/passive/BeeEntity;isInsideWaterOrBubbleColumn()Z", shift = At.Shift.AFTER))
+    private void thebumblezone_honeyFluidNotDrown(CallbackInfo ci){
+        if(this.ticksInsideWater >= 19 && this.fluidHeight.get(BzFluidTags.BZ_HONEY_FLUID) > 0){
+            this.ticksInsideWater = 2;
+        }
+    }
+}
