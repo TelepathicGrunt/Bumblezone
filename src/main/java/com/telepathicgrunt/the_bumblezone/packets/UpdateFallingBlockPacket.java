9fa25ad590b124ae457bfd1ac53ec7d1eed8a1c1
TelepathicGrunt <alexsarnese@gmail.com>
2021-12-18 11:48:50 -0500

    cleaned up packet a bit

--- src/main/java/com/telepathicgrunt/the_bumblezone/packets/UpdateFallingBlockPacket.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/packets/UpdateFallingBlockPacket.java
@@ -1,37 +1,22 @@
 package com.telepathicgrunt.the_bumblezone.packets;
 
-import com.mojang.serialization.DynamicOps;
 import com.telepathicgrunt.the_bumblezone.blocks.PileOfPollen;
 import com.telepathicgrunt.the_bumblezone.mixin.blocks.FallingBlockEntityAccessor;
 import com.telepathicgrunt.the_bumblezone.modinit.BzBlocks;
 import com.telepathicgrunt.the_bumblezone.utils.MessageHandler;
-import com.telepathicgrunt.the_bumblezone.world.dimension.layer.vanilla.Layer;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.BlockPos;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.item.FallingBlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.network.PacketDistributor;
 
 import java.util.function.Supplier;
 
-public class UpdateFallingBlockPacket {
-
-    private final int fallingBlockId;
-    private final short layer;
+public record UpdateFallingBlockPacket(int fallingBlockId, short layer) {
 
     public static void sendToClient(Entity entity, int fallingBlockId, short layer) {
         MessageHandler.DEFAULT_CHANNEL.send(PacketDistributor.TRACKING_ENTITY.with(() -> entity), new UpdateFallingBlockPacket(fallingBlockId, layer));
-    }
-
-    /*
-     * Sets block location and resource location
-     */
-    public UpdateFallingBlockPacket(int fallingBlockId, short layer) {
-        this.fallingBlockId = fallingBlockId;
-        this.layer = layer;
     }
 
     /*
@@ -57,8 +42,8 @@ public class UpdateFallingBlockPacket {
         public static void handle(final UpdateFallingBlockPacket pkt, final Supplier<NetworkEvent.Context> ctx) {
             Minecraft.getInstance().execute(() -> {
                 Entity entity = Minecraft.getInstance().level.getEntity(pkt.fallingBlockId);
-                if(entity instanceof FallingBlockEntity fallingBlockEntity && fallingBlockEntity.getBlockState().is(BzBlocks.PILE_OF_POLLEN.get())) {
-                    ((FallingBlockEntityAccessor)fallingBlockEntity).bumblezone_setBlockState(BzBlocks.PILE_OF_POLLEN.get().defaultBlockState().setValue(PileOfPollen.LAYERS, (int)pkt.layer));
+                if (entity instanceof FallingBlockEntity fallingBlockEntity && fallingBlockEntity.getBlockState().is(BzBlocks.PILE_OF_POLLEN.get())) {
+                    ((FallingBlockEntityAccessor) fallingBlockEntity).bumblezone_setBlockState(BzBlocks.PILE_OF_POLLEN.get().defaultBlockState().setValue(PileOfPollen.LAYERS, (int) pkt.layer));
                 }
             });
             ctx.get().setPacketHandled(true);
