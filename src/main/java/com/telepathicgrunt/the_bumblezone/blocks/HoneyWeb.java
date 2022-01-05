a3a3bd77c9205f69198eb0fccce3acf25f9b8d1c
TelepathicGrunt <alexsarnese@gmail.com>
2022-01-04 20:29:05 -0500

    part 2 of honey web

--- src/main/java/com/telepathicgrunt/the_bumblezone/blocks/HoneyWeb.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/blocks/HoneyWeb.java
@@ -1,16 +1,25 @@
 package com.telepathicgrunt.the_bumblezone.blocks;
 
 import com.telepathicgrunt.the_bumblezone.entities.mobs.BeehemothEntity;
+import com.telepathicgrunt.the_bumblezone.entities.nonliving.PollenPuffEntity;
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.util.Mth;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Bee;
+import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -19,6 +28,9 @@ import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.BooleanOp;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
@@ -30,75 +42,190 @@ public class HoneyWeb extends Block {
 
     public static final BooleanProperty NORTHSOUTH = BooleanProperty.create("northsouth");
     public static final BooleanProperty EASTWEST = BooleanProperty.create("eastwest");
+    public static final BooleanProperty UPDOWN = BooleanProperty.create("updown");
+    protected final VoxelShape[] collisionShapeByIndex;
+    protected final VoxelShape[] shapeByIndex;
+    private final Object2IntMap<BlockState> stateToIndex = new Object2IntOpenHashMap<>();
 
     public HoneyWeb() {
         super(Properties.of(Material.WEB, MaterialColor.COLOR_ORANGE).noCollission().requiresCorrectToolForDrops().strength(4.0F));
+        this.collisionShapeByIndex = this.makeShapes();
+        this.shapeByIndex = this.makeShapes();
         this.registerDefaultState(this.stateDefinition.any()
-                .setValue(NORTHSOUTH, true)
-                .setValue(EASTWEST, true));
+                .setValue(NORTHSOUTH, false)
+                .setValue(EASTWEST, false)
+                .setValue(UPDOWN, false));
     }
 
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> blockStateBuilder) {
-        blockStateBuilder.add(NORTHSOUTH, EASTWEST);
+        blockStateBuilder.add(NORTHSOUTH, EASTWEST, UPDOWN);
+    }
+
+    protected VoxelShape[] makeShapes() {
+        VoxelShape voxelshape1 = Block.box(7, 0, 0, 9, 16, 16);
+        VoxelShape voxelshape2 = Block.box(0, 0, 7, 16, 16, 9);
+        VoxelShape voxelshape3 = Block.box(0, 7, 0, 16, 9, 16);
+
+        return new VoxelShape[]{
+                Shapes.empty(),
+                voxelshape1,
+                voxelshape2,
+                Shapes.or(voxelshape1, voxelshape2),
+                voxelshape3,
+                Shapes.or(voxelshape1, voxelshape3),
+                Shapes.or(voxelshape2, voxelshape3),
+                Shapes.or(Shapes.or(voxelshape1, voxelshape2), voxelshape3)
+        };
+    }
+
+    protected int getAABBIndex(BlockState blockState) {
+        return this.stateToIndex.computeIfAbsent(blockState, (a) -> {
+            int bitFlag = 0;
+            if (blockState.getValue(NORTHSOUTH)) {
+                bitFlag |= 1;
+            }
+
+            if (blockState.getValue(EASTWEST)) {
+                bitFlag |= (1 << 1);
+            }
+
+            if (blockState.getValue(UPDOWN)) {
+                bitFlag |= (1 << 2);
+            }
+
+            return bitFlag;
+        });
+    }
+
+    public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return this.shapeByIndex[this.getAABBIndex(blockState)];
     }
 
     public void entityInside(BlockState blockState, Level level, BlockPos blockPos, Entity entity) {
-        if(!(entity instanceof Bee || entity instanceof BeehemothEntity)) {
-            entity.makeStuckInBlock(blockState, new Vec3(0.12D, 0.03F, 0.12D));
+        if (!(entity instanceof Bee || entity instanceof BeehemothEntity)) {
+            VoxelShape shape = this.shapeByIndex[this.getAABBIndex(blockState)];
+            shape = shape.move(blockPos.getX(), blockPos.getY(), blockPos.getZ());
+            if (Shapes.joinIsNotEmpty(shape, Shapes.create(entity.getBoundingBox()), BooleanOp.AND)) {
+                double speedReduction = 0.15f;
+
+                if (entity instanceof ThrowableItemProjectile) {
+                    Vec3 deltaMovement = entity.getDeltaMovement();
+                    entity.setDeltaMovement(new Vec3(
+                            deltaMovement.x * speedReduction,
+                            deltaMovement.y * speedReduction * 2,
+                            deltaMovement.z * speedReduction));
+                }
+                else {
+                    entity.makeStuckInBlock(blockState, new Vec3(speedReduction, speedReduction * 2, speedReduction));
+                    if (entity instanceof LivingEntity livingEntity) {
+                        livingEntity.addEffect(new MobEffectInstance(
+                                MobEffects.MOVEMENT_SLOWDOWN,
+                                200,
+                                1,
+                                false,
+                                false,
+                                true));
+                    }
+                }
+            }
         }
     }
 
     public BlockState getStateForPlacement(BlockPlaceContext placeContext) {
-        BlockGetter blockgetter = placeContext.getLevel();
+        Level level = placeContext.getLevel();
         BlockPos blockpos = placeContext.getClickedPos();
-        BlockState state = getStateForSpot(blockgetter, blockpos);
-        if(state.getValue(NORTHSOUTH) || state.getValue(EASTWEST)) {
-            return state;
-        }
-        else {
-            Direction.Axis horizontalFacing = Direction.Axis.Z;
-            for(Direction direction : placeContext.getNearestLookingDirections()) {
-                if(direction.getAxis() != Direction.Axis.Y) {
-                    horizontalFacing = direction.getAxis();
-                    break;
-                }
-            }
-
-            if(horizontalFacing == Direction.Axis.Z) {
-                return this.defaultBlockState()
-                        .setValue(NORTHSOUTH, true)
-                        .setValue(EASTWEST, false);
+        BlockState state = getStateForSpot(level, blockpos);
+        if(!state.getValue(NORTHSOUTH) && !state.getValue(EASTWEST) && !state.getValue(UPDOWN)) {
+            if(placeContext.getClickedFace().getAxis() != Direction.Axis.Y) {
+                state = this.defaultBlockState().setValue(UPDOWN, true);
             }
             else {
-                return this.defaultBlockState()
-                        .setValue(NORTHSOUTH, false)
-                        .setValue(EASTWEST, true);
+                Direction.Axis horizontalFacing = Direction.Axis.Z;
+                for(Direction direction : placeContext.getNearestLookingDirections()) {
+                    if(direction.getAxis() != Direction.Axis.Y) {
+                        horizontalFacing = direction.getAxis();
+                        break;
+                    }
+                }
+
+                if(horizontalFacing == Direction.Axis.X) {
+                    state = this.defaultBlockState()
+                            .setValue(NORTHSOUTH, true)
+                            .setValue(EASTWEST, false);
+                }
+                else {
+                    state = this.defaultBlockState()
+                            .setValue(NORTHSOUTH, false)
+                            .setValue(EASTWEST, true);
+                }
             }
         }
+
+        updateNeighboringStates(level, state, blockpos);
+        return state;
     }
 
+    private void updateNeighboringStates(Level level, BlockState centerState, BlockPos blockpos) {
+        for (Direction direction : Direction.values()) {
+            BlockPos sidePos = blockpos.relative(direction);
+            BlockState neighboringBlockstate = level.getBlockState(sidePos);
+            if (neighboringBlockstate.getBlock() instanceof HoneyWeb) {
+                boolean changedState = false;
+                if (direction.getAxis() != Direction.Axis.X && centerState.getValue(NORTHSOUTH) && !neighboringBlockstate.getValue(NORTHSOUTH)) {
+                    neighboringBlockstate = neighboringBlockstate.setValue(NORTHSOUTH, true);
+                    changedState = true;
+                }
+                if (direction.getAxis() != Direction.Axis.Z && centerState.getValue(EASTWEST) && !neighboringBlockstate.getValue(EASTWEST)) {
+                    neighboringBlockstate = neighboringBlockstate.setValue(EASTWEST, true);
+                    changedState = true;
+                }
+                if (direction.getAxis() != Direction.Axis.Y && centerState.getValue(UPDOWN) && !neighboringBlockstate.getValue(UPDOWN)) {
+                    neighboringBlockstate = neighboringBlockstate.setValue(UPDOWN, true);
+                    changedState = true;
+                }
 
-    public BlockState updateShape(BlockState blockState, Direction direction, BlockState blockState1, LevelAccessor levelAccessor, BlockPos blockPos, BlockPos blockPos1) {
-        BlockState state = getStateForSpot(levelAccessor, blockPos);
-        if(state.getValue(NORTHSOUTH) || state.getValue(EASTWEST)) {
-            return state;
+                if (changedState) {
+                    level.setBlock(sidePos, neighboringBlockstate, 3);
+                }
+            }
         }
-        return blockState;
     }
 
     @NotNull
     private BlockState getStateForSpot(BlockGetter blockgetter, BlockPos blockpos) {
-        BlockState blockstateN = blockgetter.getBlockState(blockpos.north());
-        BlockState blockstateE = blockgetter.getBlockState(blockpos.east());
-        BlockState blockstateS = blockgetter.getBlockState(blockpos.south());
-        BlockState blockstateW = blockgetter.getBlockState(blockpos.west());
+        BlockState currentBlockstate = this.defaultBlockState();
 
-        boolean connectNS = blockstateN.getBlock() instanceof HoneyWeb || blockstateS.getBlock() instanceof HoneyWeb;
-        boolean connectEW = blockstateE.getBlock() instanceof HoneyWeb || blockstateW.getBlock() instanceof HoneyWeb;
+        for (Direction direction : Direction.values()) {
+            BlockState neighboringBlockstate = blockgetter.getBlockState(blockpos.relative(direction));
+            if (neighboringBlockstate.getBlock() instanceof HoneyWeb) {
+                if (direction.getAxis() == Direction.Axis.Y) {
+                    if (neighboringBlockstate.getValue(NORTHSOUTH)) {
+                        currentBlockstate = currentBlockstate.setValue(NORTHSOUTH, true);
+                    }
+                    if (neighboringBlockstate.getValue(EASTWEST)) {
+                        currentBlockstate = currentBlockstate.setValue(EASTWEST, true);
+                    }
+                }
+                else if (direction.getAxis() == Direction.Axis.X) {
+                    if (neighboringBlockstate.getValue(UPDOWN)) {
+                        currentBlockstate = currentBlockstate.setValue(UPDOWN, true);
+                    }
+                    if (neighboringBlockstate.getValue(EASTWEST)) {
+                        currentBlockstate = currentBlockstate.setValue(EASTWEST, true);
+                    }
+                }
+                else if (direction.getAxis() == Direction.Axis.Z) {
+                    if (neighboringBlockstate.getValue(UPDOWN)) {
+                        currentBlockstate = currentBlockstate.setValue(UPDOWN, true);
+                    }
+                    if (neighboringBlockstate.getValue(NORTHSOUTH)) {
+                        currentBlockstate = currentBlockstate.setValue(NORTHSOUTH, true);
+                    }
+                }
+            }
+        }
 
-        return this.defaultBlockState()
-                .setValue(NORTHSOUTH, connectNS)
-                .setValue(EASTWEST, connectEW);
+        return currentBlockstate;
     }
 
     /**
