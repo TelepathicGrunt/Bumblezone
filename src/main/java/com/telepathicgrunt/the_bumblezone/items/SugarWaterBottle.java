caea510610ab9a879d7200fca2ca67f8ccb5a601
TelepathicGrunt <alexsarnese@gmail.com>
2022-02-12 23:04:22 -0500

    fixed sugar water bottle curing poison
    https://github.com/TelepathicGrunt/Bumblezone/issues/104

---
+++ src/main/java/com/telepathicgrunt/the_bumblezone/items/SugarWaterBottle.java
@@ -0,0 +1,72 @@
+package com.telepathicgrunt.the_bumblezone.items;
+
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemUtils;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.level.Level;
+
+public class SugarWaterBottle extends Item {
+
+    public SugarWaterBottle(Item.Properties properties) {
+        super(properties);
+    }
+
+    public ItemStack finishUsingItem(ItemStack itemStack, Level level, LivingEntity livingEntity) {
+        super.finishUsingItem(itemStack, level, livingEntity);
+        if (livingEntity instanceof ServerPlayer serverplayer) {
+            CriteriaTriggers.CONSUME_ITEM.trigger(serverplayer, itemStack);
+            serverplayer.awardStat(Stats.ITEM_USED.get(this));
+        }
+
+        if (itemStack.isEmpty()) {
+            return new ItemStack(Items.GLASS_BOTTLE);
+        }
+        else {
+            if (livingEntity instanceof Player player && !((Player)livingEntity).getAbilities().instabuild) {
+                ItemStack itemstack = new ItemStack(Items.GLASS_BOTTLE);
+                if (!player.getInventory().add(itemstack)) {
+                    player.drop(itemstack, false);
+                }
+            }
+
+            return itemStack;
+        }
+    }
+
+    /**
+     * How long it takes to use or consume an item
+     */
+    public int getUseDuration(ItemStack itemStack) {
+        return 40;
+    }
+
+    /**
+     * returns the action that specifies what animation to play when the items is being used
+     */
+    public UseAnim getUseAnimation(ItemStack itemStack) {
+        return UseAnim.DRINK;
+    }
+
+    public SoundEvent getDrinkingSound() {
+        return SoundEvents.HONEY_DRINK;
+    }
+
+    public SoundEvent getEatingSound() {
+        return SoundEvents.HONEY_DRINK;
+    }
+
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
+        return ItemUtils.startUsingInstantly(level, player, hand);
+    }
+}
