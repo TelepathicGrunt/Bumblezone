980fb6e5576f4a5730907b3ad76a82e40094eafc
TelepathicGrunt <alexsarnese@gmail.com>
2022-02-17 23:25:13 -0500

    colored honey cocoon screen

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,8 @@ Honey Cocoon now will lose items much faster when waterlogged with water above a
 When Honey Cocoon consumes specific honey items to revive Empty Honeycomb Blocks inside, the ejected container of the consumed item will be dropped
  (Example: glass bottle for consumed potions if you added potions to the bee_feeding_items item tag)
 
+Honey Cocoon's inventory screen is now orange.
+
 ##### Recipes:
 Improved the the_bumblezone:container_shapeless_recipe_bz recipe type so that it converts certain vanilla items into their containers properly.
  Example: Powder Snow Bucket does not have a container set so I have to hardcode that it should be turned to an empty bucket if used with this recipe type.
--- src/main/java/com/telepathicgrunt/the_bumblezone/screens/StrictChestScreen.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/screens/StrictChestScreen.java
@@ -2,6 +2,7 @@ package com.telepathicgrunt.the_bumblezone.screens;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.telepathicgrunt.the_bumblezone.Bumblezone;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.MenuAccess;
 import net.minecraft.client.renderer.GameRenderer;
@@ -10,7 +11,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 public class StrictChestScreen extends AbstractContainerScreen<StrictChestMenu> implements MenuAccess<StrictChestMenu> {
-    private static final ResourceLocation CONTAINER_BACKGROUND = new ResourceLocation("textures/gui/container/generic_54.png");
+    private static final ResourceLocation CONTAINER_BACKGROUND = new ResourceLocation(Bumblezone.MODID, "textures/gui/container/bee_themed_54.png");
     private final int containerRows;
 
     public StrictChestScreen(StrictChestMenu menu, Inventory playerInventory, Component title) {
