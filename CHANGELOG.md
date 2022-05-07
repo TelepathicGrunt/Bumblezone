ab60ec8ab8fd22c82de04e5dc383424c4986aa68
TelepathicGrunt <alexsarnese@gmail.com>
2022-05-06 22:28:55 -0400

    fixed keybinds

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-### **(V.5.0.11 Changes) (1.18.2 Minecraft)**
+### **(V.5.0.12 Changes) (1.18.2 Minecraft)**
 
 ##### Misc:
 Ported to Quilt Modloader
--- src/main/java/com/telepathicgrunt/the_bumblezone/client/BeehemothControls.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/client/BeehemothControls.java
@@ -1,7 +1,9 @@
 package com.telepathicgrunt.the_bumblezone.client;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import com.telepathicgrunt.the_bumblezone.Bumblezone;
 import com.telepathicgrunt.the_bumblezone.entities.mobs.BeehemothEntity;
+import com.telepathicgrunt.the_bumblezone.mixin.client.KeyMappingAccessor;
 import com.telepathicgrunt.the_bumblezone.packets.BeehemothControlsPacket;
 import io.netty.buffer.Unpooled;
 import net.minecraft.client.KeyMapping;
@@ -11,20 +13,29 @@ import org.lwjgl.glfw.GLFW;
 import org.quiltmc.qsl.networking.api.client.ClientPlayNetworking;
 
 public class BeehemothControls {
-    public static final KeyMapping KEY_BIND_BEEHEMOTH_DOWN = new KeyMapping(
-    "key." + Bumblezone.MODID + ".beehemoth_down", GLFW.GLFW_KEY_CAPS_LOCK, "key.categories." + Bumblezone.MODID
-    );
 
-    public static final KeyMapping KEY_BIND_BEEHEMOTH_UP = new KeyMapping(
-    "key." + Bumblezone.MODID + ".beehemoth_up", GLFW.GLFW_KEY_SPACE, "key.categories." + Bumblezone.MODID
-    );
+    public static final KeyMapping KEY_BIND_BEEHEMOTH_DOWN = createSafeKeyMapping("key." + Bumblezone.MODID + ".beehemoth_down", GLFW.GLFW_KEY_CAPS_LOCK);
+    public static final KeyMapping KEY_BIND_BEEHEMOTH_UP = createSafeKeyMapping("key." + Bumblezone.MODID + ".beehemoth_up", GLFW.GLFW_KEY_SPACE);
 
-    public static void keyInput(int key, int action) {
-        if (Minecraft.getInstance().player != null && Minecraft.getInstance().player.getVehicle() instanceof BeehemothEntity) {
+    public static void keyInput(int key, int scancode, int action) {
+        if (Minecraft.getInstance().player != null &&
+                Minecraft.getInstance().player.getVehicle() instanceof BeehemothEntity &&
+                (KEY_BIND_BEEHEMOTH_UP.matches(key, scancode) ||
+                        KEY_BIND_BEEHEMOTH_DOWN.matches(key, scancode)))
+        {
             FriendlyByteBuf passedData = new FriendlyByteBuf(Unpooled.buffer());
-            passedData.writeByte(key == KEY_BIND_BEEHEMOTH_UP.getDefaultKey().getValue() ? action : 2);
-            passedData.writeByte(key == KEY_BIND_BEEHEMOTH_DOWN.getDefaultKey().getValue() ? action : 2);
+            passedData.writeByte(KEY_BIND_BEEHEMOTH_UP.matches(key, scancode) ? action : 2);
+            passedData.writeByte(KEY_BIND_BEEHEMOTH_DOWN.matches(key, scancode) ? action : 2);
             ClientPlayNetworking.send(BeehemothControlsPacket.PACKET_ID, passedData);
         }
+    }
+
+    private static KeyMapping createSafeKeyMapping(String description, int keycode) {
+        InputConstants.Key key = InputConstants.Type.KEYSYM.getOrCreate(keycode);
+        KeyMapping oldMapping = KeyMappingAccessor.getMAP().get(key);
+        KeyMapping keyMapping = new KeyMapping(description, keycode, "key.categories." + Bumblezone.MODID);
+        KeyMappingAccessor.getMAP().put(key, oldMapping);
+        KeyMappingAccessor.getALL().remove(description);
+        return keyMapping;
     }
 }
