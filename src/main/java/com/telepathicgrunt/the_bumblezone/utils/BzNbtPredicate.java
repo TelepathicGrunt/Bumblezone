c017f672f7a9abb1246f84c73577b4b6232ca96b
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-09-27 15:51:43 -0400

    more compat

--- src/main/java/com/telepathicgrunt/the_bumblezone/utils/BzNbtPredicate.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/utils/BzNbtPredicate.java
@@ -4,11 +4,14 @@ package com.telepathicgrunt.the_bumblezone.utils;
 import com.mojang.serialization.Codec;
 import net.minecraft.advancements.critereon.NbtPredicate;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.nbt.NumericTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
 // Source: https://github.com/Resourceful-Bees/ResourcefulLib/blob/master/common/src/main/java/com/teamresourceful/resourcefullib/common/codecs/predicates/NbtPredicate.java
 public record BzNbtPredicate(CompoundTag tag) {
@@ -25,7 +28,7 @@ public record BzNbtPredicate(CompoundTag tag) {
     }
 
     public boolean matches(Tag tag) {
-        return tag == null ? this == ANY : this.tag == null || NbtUtils.compareNbt(this.tag, tag, true);
+        return tag == null ? this == ANY : this.tag == null || compareNbt(this.tag, tag, true);
     }
 
     public static CompoundTag getEntityTagToCompare(Entity entity) {
@@ -40,4 +43,48 @@ public record BzNbtPredicate(CompoundTag tag) {
         return compoundtag;
     }
 
+    public static boolean compareNbt(@Nullable Tag tag, @Nullable Tag tag2, boolean bl) {
+        if (tag == tag2) {
+            return true;
+        }
+        if (tag == null) {
+            return true;
+        }
+        if (tag2 == null) {
+            return false;
+        }
+        if (tag instanceof NumericTag numericTag && tag2 instanceof NumericTag numericTag2) {
+            return numericTag.getAsDouble() == numericTag2.getAsDouble();
+        }
+        if (!tag.getClass().equals(tag2.getClass())) {
+            return false;
+        }
+        if (tag instanceof CompoundTag compoundTag) {
+            CompoundTag compoundTag2 = (CompoundTag)tag2;
+            for (String string : compoundTag.getAllKeys()) {
+                Tag tag3 = compoundTag.get(string);
+                if (compareNbt(tag3, compoundTag2.get(string), bl)) continue;
+                return false;
+            }
+            return true;
+        }
+        if (tag instanceof ListTag listTag && bl) {
+            ListTag listTag2 = (ListTag)tag2;
+            if (listTag.isEmpty()) {
+                return listTag2.isEmpty();
+            }
+            for (Tag tag4 : listTag) {
+                boolean bl2 = false;
+                for (Tag value : listTag2) {
+                    if (!compareNbt(tag4, value, true)) continue;
+                    bl2 = true;
+                    break;
+                }
+                if (bl2) continue;
+                return false;
+            }
+            return true;
+        }
+        return tag.equals(tag2);
+    }
 }
