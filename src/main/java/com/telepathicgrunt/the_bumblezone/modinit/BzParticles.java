3d7f6a98cd7665af81c533383f2131356ce73efb
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-10-21 20:06:50 -0400

    renamed pollen particle file

--- src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzParticles.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzParticles.java
@@ -13,7 +13,7 @@ public class BzParticles {
     public static SimpleParticleType SPARKLE_PARTICLE = FabricParticleTypes.simple();
 
     public static void registerParticles() {
-        Registry.register(Registry.PARTICLE_TYPE, new ResourceLocation(Bumblezone.MODID, "pollen_puff"), POLLEN_PARTICLE);
+        Registry.register(Registry.PARTICLE_TYPE, new ResourceLocation(Bumblezone.MODID, "pollen_puff_particle"), POLLEN_PARTICLE);
         Registry.register(Registry.PARTICLE_TYPE, new ResourceLocation(Bumblezone.MODID, "honey_particle"), HONEY_PARTICLE);
         Registry.register(Registry.PARTICLE_TYPE, new ResourceLocation(Bumblezone.MODID, "royal_jelly_particle"), ROYAL_JELLY_PARTICLE);
         Registry.register(Registry.PARTICLE_TYPE, new ResourceLocation(Bumblezone.MODID, "sparkle_particle"), SPARKLE_PARTICLE);
