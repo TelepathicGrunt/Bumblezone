03c4b6fe5fbf261572a8154e0d3f89db34a7078b
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-10-21 20:48:03 -0400

    Some small fixes

--- src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzParticles.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzParticles.java
@@ -10,8 +10,8 @@ import net.minecraftforge.registries.RegistryObject;
 public class BzParticles {
     public static final DeferredRegister<ParticleType<?>> PARTICLE_TYPES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, Bumblezone.MODID);
 
-    public static final RegistryObject<SimpleParticleType> POLLEN_PARTICLE = PARTICLE_TYPES.register("pollen_puff", () -> new SimpleParticleType(false));
-    public static final RegistryObject<SimpleParticleType> HONEY_PARTICLE = PARTICLE_TYPES.register("honey_particle_particle", () -> new SimpleParticleType(false));
+    public static final RegistryObject<SimpleParticleType> POLLEN_PARTICLE = PARTICLE_TYPES.register("pollen_puff_particle", () -> new SimpleParticleType(false));
+    public static final RegistryObject<SimpleParticleType> HONEY_PARTICLE = PARTICLE_TYPES.register("honey_particle", () -> new SimpleParticleType(false));
     public static final RegistryObject<SimpleParticleType> SPARKLE_PARTICLE = PARTICLE_TYPES.register("sparkle_particle", () -> new SimpleParticleType(false));
     public static final RegistryObject<SimpleParticleType> ROYAL_JELLY_PARTICLE = PARTICLE_TYPES.register("royal_jelly_particle", () -> new SimpleParticleType(false));
 }
