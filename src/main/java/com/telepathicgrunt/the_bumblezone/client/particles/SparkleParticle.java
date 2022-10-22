cac26c1a22f50b5f6135e5f532099b51b91edcf2
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-10-21 20:05:45 -0400

    fixed cocoon gui and added missing structure translation

--- src/main/java/com/telepathicgrunt/the_bumblezone/client/particles/SparkleParticle.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/client/particles/SparkleParticle.java
@@ -25,7 +25,6 @@ public class SparkleParticle extends SimpleAnimatedParticle {
         this.setLocationFromBoundingbox();
     }
 
-    @OnlyIn(Dist.CLIENT)
     public static class Factory implements ParticleProvider<SimpleParticleType> {
         private final SpriteSet sprites;
 
