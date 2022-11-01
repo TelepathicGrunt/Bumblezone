f3f1cb1540c90b9570f753e448a355d403a69d00
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-11-01 18:35:48 -0400

    texture adjustments

--- src/main/java/com/telepathicgrunt/the_bumblezone/client/bakedmodel/PorousHoneycombBlockModel.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/client/bakedmodel/PorousHoneycombBlockModel.java
@@ -23,19 +23,26 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.client.model.IDynamicBakedModel;
 import net.minecraftforge.client.model.data.ModelData;
-import net.minecraftforge.client.model.data.ModelDataManager;
 import net.minecraftforge.client.model.data.ModelProperty;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.function.Function;
 
 public class PorousHoneycombBlockModel implements IDynamicBakedModel {
-    public static final ModelProperty<List<Direction>> DIRECTION_OF_HONEY_MERGERS = new ModelProperty<>();
+    public static final ModelProperty<Map<Direction, HashSet<CORNERS>>> DIRECTION_OF_HONEY_MERGERS = new ModelProperty<>();
     protected final BakedModel mainModel;
-    private final Map<List<Direction>, BakedModel> cache = Maps.newHashMap();
+    private final Map<List<CORNERS>, BakedModel> cache = Maps.newHashMap();
+    private enum CORNERS {
+        TOP_LEFT,
+        TOP_RIGHT,
+        BOTTOM_LEFT,
+        BOTTOM_RIGHT
+    }
 
     private PorousHoneycombBlockModel(BakedModel mainModel) {
         this.mainModel = mainModel;
@@ -46,18 +53,21 @@ public class PorousHoneycombBlockModel implements IDynamicBakedModel {
     public List<BakedQuad> getQuads(BlockState state, Direction side, RandomSource rand, ModelData extraData, RenderType renderType) {
         List<BakedQuad> quads = Lists.newArrayList();
         quads.addAll(mainModel.getQuads(state, side, rand, extraData, renderType));
-        if (state != null && side != null && extraData.has(DIRECTION_OF_HONEY_MERGERS) && extraData.get(DIRECTION_OF_HONEY_MERGERS) != null) {
-            List<Direction> directionsOfHoney = extraData.get(DIRECTION_OF_HONEY_MERGERS);
+
+        if (quads.size() > 0 &&
+            side != null &&
+            state != null &&
+            extraData.has(DIRECTION_OF_HONEY_MERGERS) &&
+            extraData.get(DIRECTION_OF_HONEY_MERGERS) != null)
+        {
+            Map<Direction, HashSet<CORNERS>> directionsOfHoney = extraData.get(DIRECTION_OF_HONEY_MERGERS);
 
             // For sides for face to connect texture to
-            for (Direction honeyDirection : directionsOfHoney) {
-                if (side == honeyDirection || side.getOpposite() == honeyDirection) {
-                    continue;
-                }
+            for (CORNERS corners : directionsOfHoney.get(side)) {
 
                 TextureAtlasSprite textureAtlasSprite = Minecraft.getInstance().getModelManager()
                         .getAtlas(TextureAtlas.LOCATION_BLOCKS)
-                        .getSprite(new ResourceLocation(Bumblezone.MODID, "textures/block/porous_honeycomb_block/porous_honeycomb_block_1d.png"));
+                        .getSprite(new ResourceLocation(Bumblezone.MODID, "textures/block/porous_honeycomb_block_corner_overlays/"+corners.name().toLowerCase(Locale.ROOT)+".png"));
 
                 // draw connecting texture on top of previous quad?
                 BakedQuad newBakedQuad = new BakedQuad(
@@ -68,7 +78,7 @@ public class PorousHoneycombBlockModel implements IDynamicBakedModel {
                         true
                     );
 
-                quads.set(0, newBakedQuad);
+                quads.add(newBakedQuad);
             }
         }
 
@@ -81,18 +91,97 @@ public class PorousHoneycombBlockModel implements IDynamicBakedModel {
         if (state != null) {
             if (!currentData.has(DIRECTION_OF_HONEY_MERGERS) || currentData.get(DIRECTION_OF_HONEY_MERGERS) == null) {
                 currentData = ModelData.builder()
-                        .with(DIRECTION_OF_HONEY_MERGERS, new ArrayList<>())
+                        .with(DIRECTION_OF_HONEY_MERGERS, new HashMap<>())
                         .build();
             }
 
             BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos();
-            currentData.get(DIRECTION_OF_HONEY_MERGERS).clear();
-
             for (Direction direction : Direction.values()) {
-                mutableBlockPos.set(pos).move(direction);
-                BlockState neighborState = level.getBlockState(mutableBlockPos);
-                if (neighborState.is(BzBlocks.FILLED_POROUS_HONEYCOMB.get()) || neighborState.is(BzBlocks.HONEYCOMB_BROOD.get())) {
-                    currentData.get(DIRECTION_OF_HONEY_MERGERS).add(direction);
+                currentData.get(DIRECTION_OF_HONEY_MERGERS).put(direction, new HashSet<>());
+            }
+
+            for (int x = -1; x <= 1; x++) {
+                for (int y = -1; y <= 1; y++) {
+                    for (int z = -1; z <= 1; z++) {
+                        int absSum = Math.abs(x) + Math.abs(y) + Math.abs(z);
+                        if (absSum == 3 || absSum == 0) {
+                            continue;
+                        }
+
+                        mutableBlockPos.set(pos).move(x, y, z);
+                        BlockState neighborState = level.getBlockState(mutableBlockPos);
+                        if (neighborState.is(BzBlocks.FILLED_POROUS_HONEYCOMB.get()) || neighborState.is(BzBlocks.HONEYCOMB_BROOD.get())) {
+                            for (Direction direction : Direction.values()) {
+                                if (Math.abs(direction.getStepX()) == Math.abs(x) &&
+                                    Math.abs(direction.getStepY()) == Math.abs(y) &&
+                                    Math.abs(direction.getStepZ()) == Math.abs(z))
+                                {
+                                    continue;
+                                }
+
+                                if (x == -1 && z == -1) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.BOTTOM_LEFT);
+                                }
+                                else if (x == -1 && z == 0) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.BOTTOM_LEFT);
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.TOP_LEFT);
+                                }
+                                else if (x == -1 && z == 1) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.TOP_LEFT);
+                                }
+                                else if (x == 1 && z == -1) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.BOTTOM_RIGHT);
+                                }
+                                else if (x == 1 && z == 0) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.BOTTOM_RIGHT);
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.TOP_RIGHT);
+                                }
+                                else if (x == 1 && z == 1) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.TOP_RIGHT);
+                                }
+                                else if (x == -1 && y == -1) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.BOTTOM_LEFT);
+                                }
+                                else if (x == -1 && y == 0) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.BOTTOM_LEFT);
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.TOP_LEFT);
+                                }
+                                else if (x == -1 && y == 1) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.TOP_LEFT);
+                                }
+                                else if (x == 1 && y == -1) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.BOTTOM_RIGHT);
+                                }
+                                else if (x == 1 && y == 0) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.BOTTOM_RIGHT);
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.TOP_RIGHT);
+                                }
+                                else if (x == 1 && y == 1) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.TOP_RIGHT);
+                                }
+                                else if (y == -1 && z == -1) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.BOTTOM_LEFT);
+                                }
+                                else if (y == -1 && z == 0) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.BOTTOM_LEFT);
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.TOP_LEFT);
+                                }
+                                else if (y == -1 && z == 1) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.TOP_LEFT);
+                                }
+                                else if (y == 1 && z == -1) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.BOTTOM_RIGHT);
+                                }
+                                else if (y == 1 && z == 0) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.BOTTOM_RIGHT);
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.TOP_RIGHT);
+                                }
+                                else if (y == 1 && z == 1) {
+                                    currentData.get(DIRECTION_OF_HONEY_MERGERS).get(direction).add(CORNERS.TOP_RIGHT);
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
