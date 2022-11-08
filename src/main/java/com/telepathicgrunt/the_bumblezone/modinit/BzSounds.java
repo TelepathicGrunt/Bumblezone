88f9f8b8ea78d69ebe437e41930cc454bee2acd6
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-11-07 21:06:48 -0500

    part 2 of new music

--- src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzSounds.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzSounds.java
@@ -13,6 +13,8 @@ public class BzSounds {
     public static final RegistryObject<SoundEvent> ANGERED_BEES = registerSoundEvent("angered_bees");
     public static final RegistryObject<SoundEvent> MUSIC_DISC_HONEY_BEE_RAT_FACED_BOY = registerSoundEvent("music_disc.honey_bee_rat_faced_boy");
     public static final RegistryObject<SoundEvent> MUSIC_DISC_FLIGHT_OF_THE_BUMBLEBEE_RIMSKY_KORSAKOV = registerSoundEvent("music_disc.flight_of_the_bumblebee_rimsky_korsakov");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_LA_BEE_DA_LOCA = registerSoundEvent("music_disc.la_bee_da_loca");
+    public static final RegistryObject<SoundEvent> MUSIC_DISC_BEE_LAXING_WITH_THE_HOM_BEES = registerSoundEvent("music_disc.bee_laxing_with_the_hom_bees");
     public static final RegistryObject<SoundEvent> BEEHEMOTH_HURT = registerSoundEvent("entity.the_bumblezone.beehemoth.hurt");
     public static final RegistryObject<SoundEvent> BEEHEMOTH_DEATH = registerSoundEvent("entity.the_bumblezone.beehemoth.death");
     public static final RegistryObject<SoundEvent> BEEHEMOTH_LOOP = registerSoundEvent("entity.the_bumblezone.beehemoth.loop");
