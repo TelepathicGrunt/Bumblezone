82ce3d73bcc88920f532783ad6f208f0d26a7649
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-11-08 19:19:26 -0500

    added queen desire commands

--- src/main/java/com/telepathicgrunt/the_bumblezone/commands/NoneOpCommands.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/commands/NoneOpCommands.java
@@ -4,7 +4,10 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.tree.LiteralCommandNode;
+import com.telepathicgrunt.the_bumblezone.capabilities.BzCapabilities;
+import com.telepathicgrunt.the_bumblezone.capabilities.EntityMisc;
 import com.telepathicgrunt.the_bumblezone.items.EssenceOfTheBees;
+import com.telepathicgrunt.the_bumblezone.modinit.BzCriterias;
 import net.minecraft.commands.CommandBuildContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
@@ -26,6 +29,16 @@ public class NoneOpCommands {
     private static MinecraftServer currentMinecraftServer = null;
     private static Set<String> cachedSuggestion = new HashSet<>();
     public static final String IS_BEE_ESSENCED_METHOD = "is_bee_essenced";
+    public static final String QUEENS_DESIRED_CRAFTED_BEEHIVE_METHOD = "queens_desired_crafted_beehive";
+    public static final String QUEENS_DESIRED_BEES_BRED_METHOD = "queens_desired_bees_bred";
+    public static final String QUEENS_DESIRED_FLOWERS_SPAWNED_METHOD = "queens_desired_flowers_spawned";
+    public static final String QUEENS_DESIRED_HONEY_BOTTLE_DRANK_METHOD = "queens_desired_honey_bottle_drank";
+    public static final String QUEENS_DESIRED_BEE_STINGERS_FIRED_METHOD = "queens_desired_bee_stingers_fired";
+    public static final String QUEENS_DESIRED_BEE_SAVED_METHOD = "queens_desired_bee_saved";
+    public static final String QUEENS_DESIRED_POLLEN_PUFF_HITS_METHOD = "queens_desired_pollen_puff_hits";
+    public static final String QUEENS_DESIRED_HONEY_SLIME_BRED_METHOD = "queens_desired_honey_slime_bred";
+    public static final String QUEENS_DESIRED_BEES_FED_METHOD = "queens_desired_bees_fed";
+    public static final String QUEENS_DESIRED_QUEEN_BEE_TRADE_METHOD = "queens_desired_queen_bee_trade";
 
     public static void createCommand(CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext buildContext) {
         String commandString = "bumblezone";
@@ -51,6 +64,16 @@ public class NoneOpCommands {
 
         Set<String> suggestedStrings = new HashSet<>();
         suggestedStrings.add(IS_BEE_ESSENCED_METHOD);
+        suggestedStrings.add(QUEENS_DESIRED_CRAFTED_BEEHIVE_METHOD);
+        suggestedStrings.add(QUEENS_DESIRED_BEES_BRED_METHOD);
+        suggestedStrings.add(QUEENS_DESIRED_FLOWERS_SPAWNED_METHOD);
+        suggestedStrings.add(QUEENS_DESIRED_HONEY_BOTTLE_DRANK_METHOD);
+        suggestedStrings.add(QUEENS_DESIRED_BEE_STINGERS_FIRED_METHOD);
+        suggestedStrings.add(QUEENS_DESIRED_BEE_SAVED_METHOD);
+        suggestedStrings.add(QUEENS_DESIRED_POLLEN_PUFF_HITS_METHOD);
+        suggestedStrings.add(QUEENS_DESIRED_HONEY_SLIME_BRED_METHOD);
+        suggestedStrings.add(QUEENS_DESIRED_BEES_FED_METHOD);
+        suggestedStrings.add(QUEENS_DESIRED_QUEEN_BEE_TRADE_METHOD);
 
         currentMinecraftServer = cs.getSource().getServer();
         cachedSuggestion = suggestedStrings;
@@ -60,14 +83,75 @@ public class NoneOpCommands {
     public static void runMethod(String method, CommandContext<CommandSourceStack> cs) {
         Player player = cs.getSource().getEntity() instanceof Player player1 ? player1 : null;
         if (player instanceof ServerPlayer serverPlayer) {
+
             if (method.equals(IS_BEE_ESSENCED_METHOD)) {
                 boolean hasBeeEssence = EssenceOfTheBees.hasEssence(serverPlayer);
                 MutableComponent mutableComponent = Component.translatable(
                         hasBeeEssence ?
                                 "command.the_bumblezone.have_bee_essence" :
-                                "command.the_bumblezone.dow_not_have_bee_essence"
+                                "command.the_bumblezone.does_not_have_bee_essence"
                 );
                 player.displayClientMessage(mutableComponent, false);
+                return;
+            }
+
+            if (!EntityMisc.rootAdvancementDone(serverPlayer)) {
+                player.displayClientMessage(Component.translatable("command.the_bumblezone.queens_desired_not_active"), false);
+                return;
+            }
+
+            switch (method) {
+                case QUEENS_DESIRED_CRAFTED_BEEHIVE_METHOD ->
+                        serverPlayer.getCapability(BzCapabilities.ENTITY_MISC).ifPresent(capability ->
+                                player.displayClientMessage(
+                                        Component.translatable("command.the_bumblezone.queens_desired_crafted_beehive_method", capability.craftedBeehives),
+                                        false));
+                case QUEENS_DESIRED_BEES_BRED_METHOD ->
+                        serverPlayer.getCapability(BzCapabilities.ENTITY_MISC).ifPresent(capability ->
+                                player.displayClientMessage(
+                                        Component.translatable("command.the_bumblezone.queens_desired_bees_bred_method", capability.beesBred),
+                                        false));
+                case QUEENS_DESIRED_FLOWERS_SPAWNED_METHOD ->
+                        serverPlayer.getCapability(BzCapabilities.ENTITY_MISC).ifPresent(capability ->
+                                player.displayClientMessage(
+                                        Component.translatable("command.the_bumblezone.queens_desired_flowers_spawned_method", capability.flowersSpawned),
+                                        false));
+                case QUEENS_DESIRED_HONEY_BOTTLE_DRANK_METHOD ->
+                        serverPlayer.getCapability(BzCapabilities.ENTITY_MISC).ifPresent(capability ->
+                                player.displayClientMessage(
+                                        Component.translatable("command.the_bumblezone.queens_desired_honey_bottle_drank_method", capability.honeyBottleDrank),
+                                        false));
+                case QUEENS_DESIRED_BEE_STINGERS_FIRED_METHOD ->
+                        serverPlayer.getCapability(BzCapabilities.ENTITY_MISC).ifPresent(capability ->
+                                player.displayClientMessage(
+                                        Component.translatable("command.the_bumblezone.queens_desired_bee_stingers_fired_method", capability.beeStingersFired),
+                                        false));
+                case QUEENS_DESIRED_BEE_SAVED_METHOD ->
+                        serverPlayer.getCapability(BzCapabilities.ENTITY_MISC).ifPresent(capability ->
+                                player.displayClientMessage(
+                                        Component.translatable("command.the_bumblezone.queens_desired_bee_saved_method", capability.beeSaved),
+                                        false));
+                case QUEENS_DESIRED_POLLEN_PUFF_HITS_METHOD ->
+                        serverPlayer.getCapability(BzCapabilities.ENTITY_MISC).ifPresent(capability ->
+                                player.displayClientMessage(
+                                        Component.translatable("command.the_bumblezone.queens_desired_pollen_puff_hits_method", capability.pollenPuffHits),
+                                        false));
+                case QUEENS_DESIRED_HONEY_SLIME_BRED_METHOD ->
+                        serverPlayer.getCapability(BzCapabilities.ENTITY_MISC).ifPresent(capability ->
+                                player.displayClientMessage(
+                                        Component.translatable("command.the_bumblezone.queens_desired_honey_slime_bred_method", capability.honeySlimeBred),
+                                        false));
+                case QUEENS_DESIRED_BEES_FED_METHOD ->
+                        serverPlayer.getCapability(BzCapabilities.ENTITY_MISC).ifPresent(capability ->
+                                player.displayClientMessage(
+                                        Component.translatable("command.the_bumblezone.queens_desired_bees_fed_method", capability.beesFed),
+                                        false));
+                case QUEENS_DESIRED_QUEEN_BEE_TRADE_METHOD ->
+                        serverPlayer.getCapability(BzCapabilities.ENTITY_MISC).ifPresent(capability ->
+                                player.displayClientMessage(
+                                        Component.translatable("command.the_bumblezone.queens_desired_queen_bee_trade_method", capability.queenBeeTrade),
+                                        false));
+                default -> {}
             }
         }
     }
