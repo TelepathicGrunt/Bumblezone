edbbbf4a2cb78dd3fda41379968598c6607e4af8
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-11-19 13:11:15 -0500

    tried cleaning up enchantment mixins a little bit

--- src/main/java/com/telepathicgrunt/the_bumblezone/mixin/enchantments/EnchantmentMixin.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/mixin/enchantments/EnchantmentMixin.java
@@ -1,5 +1,6 @@
 package com.telepathicgrunt.the_bumblezone.mixin.enchantments;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.telepathicgrunt.the_bumblezone.items.BeeCannon;
 import com.telepathicgrunt.the_bumblezone.items.CarpenterBeeBoots;
 import com.telepathicgrunt.the_bumblezone.items.CrystalCannon;
@@ -9,33 +10,34 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Enchantment.class)
 public class EnchantmentMixin {
 
-    @Inject(method = "canEnchant",
-            at = @At(value = "HEAD"),
-            cancellable = true)
-    private void thebumblezone_isEnchantmentInvalid(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        if (HoneyCrystalShield.isInvalidForHoneyCrystalShield(stack, ((Enchantment)(Object)this))) {
-            cir.setReturnValue(false);
+    @ModifyReturnValue(method = "canEnchant",
+            at = @At(value = "RETURN"))
+    private boolean thebumblezone_isEnchantmentInvalidForBzItem(boolean canEnchant, ItemStack stack) {
+        Enchantment enchantment = ((Enchantment)(Object)this);
+
+        if (HoneyCrystalShield.isInvalidForHoneyCrystalShield(stack, enchantment)) {
+            return true;
         }
-        else if (StingerSpearItem.isInvalidForStingerSpear(stack, ((Enchantment)(Object)this))) {
-            cir.setReturnValue(false);
+        else if (StingerSpearItem.isInvalidForStingerSpear(stack, enchantment)) {
+            return true;
         }
-        else if (StingerSpearItem.canBeEnchanted(stack, ((Enchantment)(Object)this))) {
-            cir.setReturnValue(true);
+        else if (StingerSpearItem.canBeEnchanted(stack, enchantment)) {
+            return true;
         }
-        else if (CarpenterBeeBoots.canBeEnchanted(stack, ((Enchantment)(Object)this))) {
-            cir.setReturnValue(true);
+        else if (CarpenterBeeBoots.canBeEnchanted(stack, enchantment)) {
+            return true;
         }
-        else if (BeeCannon.canBeEnchanted(stack, ((Enchantment)(Object)this))) {
-            cir.setReturnValue(true);
+        else if (BeeCannon.canBeEnchanted(stack, enchantment)) {
+            return true;
         }
-        else if (CrystalCannon.canBeEnchanted(stack, ((Enchantment)(Object)this))) {
-            cir.setReturnValue(true);
+        else if (CrystalCannon.canBeEnchanted(stack, enchantment)) {
+            return true;
         }
+
+        return canEnchant;
     }
 }
