8a83c5a9fb8f9f46ce67b62520581ff4103c4bd2
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-12-03 10:47:54 -0500

    new recipe and advancement for incense candle

--- src/main/java/com/telepathicgrunt/the_bumblezone/mixin/containers/ResultSlotMixin.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/mixin/containers/ResultSlotMixin.java
@@ -1,8 +1,12 @@
 package com.telepathicgrunt.the_bumblezone.mixin.containers;
 
+import com.telepathicgrunt.the_bumblezone.blocks.IncenseCandleBase;
 import com.telepathicgrunt.the_bumblezone.components.MiscComponent;
+import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.inventory.ResultSlot;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,13 +17,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ResultSlot.class)
 public class ResultSlotMixin {
+
     @Final
     @Shadow
     private Player player;
+
+    @Final
+    @Shadow
+    private CraftingContainer craftSlots;
 
     @Inject(method = "checkTakeAchievements(Lnet/minecraft/world/item/ItemStack;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;onCraftedBy(Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;I)V"))
     private void thebumblezone_craftedItem(ItemStack itemStack, CallbackInfo ci) {
         MiscComponent.onItemCrafted(itemStack, player);
+        IncenseCandleBase.multiPotionCandleCrafted(player, itemStack, craftSlots);
     }
 }
