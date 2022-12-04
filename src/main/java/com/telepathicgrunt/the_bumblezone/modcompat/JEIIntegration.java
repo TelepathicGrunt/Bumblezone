5c2515b700ac3888c31568571e38c65f08e9ef9b
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-12-04 10:15:34 -0500

    add super candle ingredient incense candle

--- src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/JEIIntegration.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/JEIIntegration.java
@@ -116,8 +116,10 @@ public class JEIIntegration implements IModPlugin {
         ClientLevel level = Minecraft.getInstance().level;
         if (level == null)
             return;
-        level.getRecipeManager().byKey(new ResourceLocation(Bumblezone.MODID, "incense_candle"))
-                .ifPresent(recipe -> registerExtraRecipes(recipe, registration));
+		level.getRecipeManager().byKey(new ResourceLocation(Bumblezone.MODID, "incense_candle"))
+				.ifPresent(recipe -> registerExtraRecipes(recipe, registration, false));
+		level.getRecipeManager().byKey(new ResourceLocation(Bumblezone.MODID, "incense_candle_from_super_candles"))
+				.ifPresent(recipe -> registerExtraRecipes(recipe, registration, true));
     }
 
     private static void addInfo(IRecipeRegistration registration, Item item) {
@@ -134,12 +136,12 @@ public class JEIIntegration implements IModPlugin {
                 Component.translatable(Bumblezone.MODID + "." + BuiltInRegistries.FLUID.getKey(fluid).getPath() + ".jei_description"));
     }
 
-    private static void registerExtraRecipes(Recipe<?> baseRecipe, IRecipeRegistration registration) {
-        if (baseRecipe instanceof IncenseCandleRecipe incenseCandleRecipe) {
-            List<CraftingRecipe> extraRecipes = FakeIncenseCandleRecipeCreator.constructFakeRecipes(incenseCandleRecipe);
-            registration.addRecipes(RecipeTypes.CRAFTING, extraRecipes);
-        }
-    }
+	private static void registerExtraRecipes(Recipe<?> baseRecipe, IRecipeRegistration registration, boolean oneRecipeOnly) {
+		if (baseRecipe instanceof IncenseCandleRecipe incenseCandleRecipe) {
+			List<CraftingRecipe> extraRecipes = FakeIncenseCandleRecipeCreator.constructFakeRecipes(incenseCandleRecipe, oneRecipeOnly);
+			registration.addRecipes(RecipeTypes.CRAFTING, extraRecipes);
+		}
+	}
 
     private static class FluidStack implements IJeiFluidIngredient {
 
