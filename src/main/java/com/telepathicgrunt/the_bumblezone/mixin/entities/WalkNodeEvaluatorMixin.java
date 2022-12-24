4740bb2f6069c506bd863988b397fcb602bdebed
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-12-24 14:32:07 -0500

    fixed fluid pathfind and improve wrath removal

--- src/main/java/com/telepathicgrunt/the_bumblezone/mixin/entities/WalkNodeEvaluatorMixin.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/mixin/entities/WalkNodeEvaluatorMixin.java
@@ -1,9 +1,14 @@
 package com.telepathicgrunt.the_bumblezone.mixin.entities;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.telepathicgrunt.the_bumblezone.blocks.SuperCandleWick;
 import com.telepathicgrunt.the_bumblezone.modinit.BzTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
 import net.minecraft.world.level.pathfinder.WalkNodeEvaluator;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,5 +26,17 @@ public class WalkNodeEvaluatorMixin {
             }
         }
         return isBurningBlock;
+    }
+
+    @WrapOperation(method = "getBlockPathTypeRaw(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/pathfinder/BlockPathTypes;",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/material/FluidState;is(Lnet/minecraft/tags/TagKey;)Z", ordinal =  1))
+    private static boolean thebumblezone_bzFluidHazard(FluidState fluidState, TagKey<Fluid> waterTagKey, Operation<Boolean> original) {
+        boolean originalWaterCheck = original.call(fluidState, waterTagKey);
+        if (!originalWaterCheck) {
+            if (fluidState.is(BzTags.BZ_HONEY_FLUID) || fluidState.is(BzTags.ROYAL_JELLY_FLUID) || fluidState.is(BzTags.SUGAR_WATER_FLUID)) {
+                return true;
+            }
+        }
+        return originalWaterCheck;
     }
 }
