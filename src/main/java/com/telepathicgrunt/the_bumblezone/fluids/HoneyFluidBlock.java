383dd88283816d142c90c5d1eb102ac88db9f700
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-12-25 19:23:00 -0500

    redo bee pollination block interaction

--- src/main/java/com/telepathicgrunt/the_bumblezone/fluids/HoneyFluidBlock.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/fluids/HoneyFluidBlock.java
@@ -123,23 +123,12 @@ public class HoneyFluidBlock extends LiquidBlock {
     @Override
     public void entityInside(BlockState state, Level world, BlockPos position, Entity entity) {
         double verticalSpeedDeltaLimit = 0.01D;
-        if (entity instanceof Bee beeEntity) {
-            if(state.is(BzFluids.HONEY_FLUID_BLOCK) &&
-                beeEntity.hasNectar() &&
-                !state.getFluidState().isSource() &&
-                !state.getFluidState().getValue(FALLING))
-            {
-                ((BeeEntityInvoker)entity).callSetHasNectar(false);
-                world.setBlock(position, BzFluids.HONEY_FLUID.defaultFluidState().createLegacyBlock(), 3);
-            }
-
-            if (beeEntity.getHealth() < beeEntity.getMaxHealth()) {
-                float diff = beeEntity.getMaxHealth() - beeEntity.getHealth();
-                beeEntity.heal(diff);
-                BlockState currentState = world.getBlockState(position);
-                if(currentState.is(BzFluids.HONEY_FLUID_BLOCK)) {
-                    world.setBlock(position, currentState.setValue(HoneyFluidBlock.LEVEL, Math.max(currentState.getValue(HoneyFluidBlock.LEVEL) - (int)Math.ceil(diff), 1)), 3);
-                }
+        if (entity instanceof Bee beeEntity && beeEntity.getHealth() < beeEntity.getMaxHealth()) {
+            float diff = beeEntity.getMaxHealth() - beeEntity.getHealth();
+            beeEntity.heal(diff);
+            BlockState currentState = world.getBlockState(position);
+            if(currentState.is(BzFluids.HONEY_FLUID_BLOCK)) {
+                world.setBlock(position, currentState.setValue(HoneyFluidBlock.LEVEL, Math.max(currentState.getValue(HoneyFluidBlock.LEVEL) - (int)Math.ceil(diff), 1)), 3);
             }
         }
         else if(Math.abs(entity.getDeltaMovement().y()) > verticalSpeedDeltaLimit && entity.fallDistance <= 0.2D) {
