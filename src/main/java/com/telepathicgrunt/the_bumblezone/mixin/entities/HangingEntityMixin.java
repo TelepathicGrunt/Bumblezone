4b1df11551108cef543610076dc723c26fb63fe5
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-12-26 13:18:01 -0500

    changed to use wrap operation

--- src/main/java/com/telepathicgrunt/the_bumblezone/mixin/entities/HangingEntityMixin.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/mixin/entities/HangingEntityMixin.java
@@ -1,6 +1,8 @@
 
 package com.telepathicgrunt.the_bumblezone.mixin.entities;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.entity.decoration.HangingEntity;
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,10 +13,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class HangingEntityMixin {
 
     // Silence logspam that isn't an issue by lowering it from error to debug: https://bugs.mojang.com/browse/MC-252934
-    @Redirect(method = "readAdditionalSaveData(Lnet/minecraft/nbt/CompoundTag;)V",
+    @WrapOperation(method = "readAdditionalSaveData(Lnet/minecraft/nbt/CompoundTag;)V",
             at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Ljava/lang/String;Ljava/lang/Object;)V", remap = false),
             require = 0)
-    private void thebumblezone_lowerLoggingLevel(Logger instance, String s, Object o) {
+    private void thebumblezone_lowerLoggingLevel(Logger instance, String s, Object o, Operation<Void> original) {
         instance.debug(s, o);
     }
 }
