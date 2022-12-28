5eeb64f1f0cd6c8c6f35f34be535fd3d5f683e6a
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2022-12-27 20:41:39 -0500

    remove references to bee better

--- src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/ModChecker.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/ModChecker.java
@@ -10,7 +10,6 @@ public class ModChecker {
     public static boolean potionOfBeesPresent = false;
     public static boolean strangePresent = false;
     public static boolean friendsAndFoesPresent = false;
-    public static boolean beeBetterPresent = false;
 
     /*
      * -- DO NOT TURN THE LAMBDAS INTO METHOD REFS. Method refs are not classloading safe. --
@@ -28,9 +27,6 @@ public class ModChecker {
         try {
             currentModID = "friendsandfoes";
             loadupModCompat(currentModID, () -> FriendsAndFoesCompat.setupCompat());
-
-            currentModID = "beebetter";
-            //loadupModCompat(currentModID, () -> BeeBetterCompat.setupCompat());
         }
         catch (Exception e) {
             printErrorToLogs("classloading " + currentModID + " and so, mod compat done afterwards broke");
