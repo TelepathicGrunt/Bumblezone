f8b397e2d942b4d7c33f06a1ab06066ceb5c437e
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-01-02 09:36:05 -0500

    tiny fluid check performance boost

--- src/main/java/com/telepathicgrunt/the_bumblezone/mixin/blocks/WaterEquivalenceMixin.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/mixin/blocks/WaterEquivalenceMixin.java
@@ -3,6 +3,7 @@ package com.telepathicgrunt.the_bumblezone.mixin.blocks;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.telepathicgrunt.the_bumblezone.modinit.BzTags;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.level.material.WaterFluid;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +14,7 @@ public class WaterEquivalenceMixin {
     @ModifyReturnValue(method = "isSame",
             at = @At(value = "RETURN"))
     private boolean thebumblezone_isEquivalentToSugarWater(boolean isSame, Fluid fluid) {
-        if(!isSame && fluid.defaultFluidState().is(BzTags.VISUAL_WATER_FLUID)) {
+        if(!isSame && !fluid.defaultFluidState().isEmpty() && fluid.defaultFluidState().is(BzTags.VISUAL_WATER_FLUID)) {
             return true;
         }
         return isSame;
