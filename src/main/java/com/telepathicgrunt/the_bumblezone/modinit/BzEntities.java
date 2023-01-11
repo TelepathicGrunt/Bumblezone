9d274a75997211950586a9b13c2564dd1e15d0c7
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-01-11 18:53:23 -0500

    Updated to use QSL entity type builder

--- src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzEntities.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzEntities.java
@@ -8,7 +8,6 @@ import com.telepathicgrunt.the_bumblezone.entities.nonliving.BeeStingerEntity;
 import com.telepathicgrunt.the_bumblezone.entities.nonliving.HoneyCrystalShardEntity;
 import com.telepathicgrunt.the_bumblezone.entities.nonliving.PollenPuffEntity;
 import com.telepathicgrunt.the_bumblezone.entities.nonliving.ThrownStingerSpearEntity;
-import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -18,66 +17,67 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.entity.SpawnPlacements;
 import net.minecraft.world.level.levelgen.Heightmap;
+import org.quiltmc.qsl.entity.api.QuiltEntityTypeBuilder;
 import org.quiltmc.qsl.entity.networking.api.tracked_data.QuiltTrackedDataHandlerRegistry;
 
 public class BzEntities {
 
-    public static final EntityType<HoneySlimeEntity> HONEY_SLIME = FabricEntityTypeBuilder
+    public static final EntityType<HoneySlimeEntity> HONEY_SLIME = QuiltEntityTypeBuilder
             .<HoneySlimeEntity>createMob()
             .spawnGroup(MobCategory.CREATURE)
             .entityFactory(HoneySlimeEntity::new)
-            .defaultAttributes(HoneySlimeEntity::getAttributeBuilder)
-            .dimensions(EntityDimensions.fixed(1F, 1F))
-            .trackRangeChunks(8)
+            .defaultAttributes(HoneySlimeEntity.getAttributeBuilder())
+            .setDimensions(EntityDimensions.fixed(1F, 1F))
+            .maxChunkTrackingRange(8)
             .spawnRestriction(SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, Mob::checkMobSpawnRules)
             .build();
 
-    public static final EntityType<BeehemothEntity> BEEHEMOTH = FabricEntityTypeBuilder
+    public static final EntityType<BeehemothEntity> BEEHEMOTH = QuiltEntityTypeBuilder
             .<BeehemothEntity>createMob()
             .spawnGroup(MobCategory.CREATURE)
             .entityFactory(BeehemothEntity::new)
-            .defaultAttributes(BeehemothEntity::getAttributeBuilder)
-            .dimensions(EntityDimensions.fixed(1.2F, 1.2F))
-            .trackRangeChunks(16)
+            .defaultAttributes(BeehemothEntity.getAttributeBuilder())
+            .setDimensions(EntityDimensions.fixed(1.2F, 1.2F))
+            .maxChunkTrackingRange(16)
             .spawnRestriction(SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, BeehemothEntity::checkMobSpawnRules)
             .build();
 
-    public static final EntityType<BeeQueenEntity> BEE_QUEEN = FabricEntityTypeBuilder
+    public static final EntityType<BeeQueenEntity> BEE_QUEEN = QuiltEntityTypeBuilder
             .<BeeQueenEntity>createMob()
             .spawnGroup(MobCategory.CREATURE)
             .entityFactory(BeeQueenEntity::new)
-            .defaultAttributes(BeeQueenEntity::getAttributeBuilder)
-            .dimensions(EntityDimensions.fixed(2.9F, 2.9F))
-            .trackRangeChunks(16)
+            .defaultAttributes(BeeQueenEntity.getAttributeBuilder())
+            .setDimensions(EntityDimensions.fixed(2.9F, 2.9F))
+            .maxChunkTrackingRange(16)
             .spawnRestriction(SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES, BeeQueenEntity::checkMobSpawnRules)
             .build();
 
-    public static final EntityType<PollenPuffEntity> POLLEN_PUFF_ENTITY = FabricEntityTypeBuilder
+    public static final EntityType<PollenPuffEntity> POLLEN_PUFF_ENTITY = QuiltEntityTypeBuilder
             .<PollenPuffEntity>create(MobCategory.MISC, PollenPuffEntity::new)
-            .dimensions(EntityDimensions.fixed(0.25F, 0.25F))
-            .trackRangeChunks(4)
-            .trackedUpdateRate(10)
+            .setDimensions(EntityDimensions.fixed(0.25F, 0.25F))
+            .maxChunkTrackingRange(4)
+            .maxBlockTrackingRange(10)
             .build();
 
-    public static final EntityType<ThrownStingerSpearEntity> THROWN_STINGER_SPEAR_ENTITY = FabricEntityTypeBuilder.
+    public static final EntityType<ThrownStingerSpearEntity> THROWN_STINGER_SPEAR_ENTITY = QuiltEntityTypeBuilder.
             <ThrownStingerSpearEntity>create(MobCategory.MISC, ThrownStingerSpearEntity::new)
-            .dimensions(EntityDimensions.fixed(0.5F, 0.5F))
-            .trackRangeChunks(4)
-            .trackedUpdateRate(20)
+            .setDimensions(EntityDimensions.fixed(0.5F, 0.5F))
+            .maxChunkTrackingRange(4)
+            .maxBlockTrackingRange(20)
             .build();
 
-    public static final EntityType<BeeStingerEntity> BEE_STINGER_ENTITY = FabricEntityTypeBuilder.
+    public static final EntityType<BeeStingerEntity> BEE_STINGER_ENTITY = QuiltEntityTypeBuilder.
             <BeeStingerEntity>create(MobCategory.MISC, BeeStingerEntity::new)
-            .dimensions(EntityDimensions.fixed(0.5F, 0.5F))
-            .trackRangeChunks(4)
-            .trackedUpdateRate(20)
+            .setDimensions(EntityDimensions.fixed(0.5F, 0.5F))
+            .maxChunkTrackingRange(4)
+            .maxBlockTrackingRange(20)
             .build();
 
-    public static final EntityType<HoneyCrystalShardEntity> HONEY_CRYSTAL_SHARD = FabricEntityTypeBuilder.
+    public static final EntityType<HoneyCrystalShardEntity> HONEY_CRYSTAL_SHARD = QuiltEntityTypeBuilder.
             <HoneyCrystalShardEntity>create(MobCategory.MISC, HoneyCrystalShardEntity::new)
-            .dimensions(EntityDimensions.fixed(0.5F, 0.5F))
-            .trackRangeChunks(4)
-            .trackedUpdateRate(20)
+            .setDimensions(EntityDimensions.fixed(0.5F, 0.5F))
+            .maxChunkTrackingRange(4)
+            .maxBlockTrackingRange(20)
             .build();
 
     public static void registerEntities() {
