e4ac307003cbc61f49844a5d8337e177ab89bf13
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-02-07 19:13:15 -0500

    wick, xp orb, and honey compass nbt fixes

--- src/main/java/com/telepathicgrunt/the_bumblezone/blocks/SuperCandleWick.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/blocks/SuperCandleWick.java
@@ -167,7 +167,7 @@ public class SuperCandleWick extends Block implements SimpleWaterloggedBlock {
             if (entityInSpace && (BzGeneralConfigs.superCandlesBurnsMobs.get() || isProjectile)) {
                 if (!entity.fireImmune()) {
                     entity.setRemainingFireTicks(entity.getRemainingFireTicks() + 1);
-                    if (entity.getRemainingFireTicks() == 0) {
+                    if (entity.getRemainingFireTicks() <= 0) {
                         entity.setSecondsOnFire(1);
                     }
 
