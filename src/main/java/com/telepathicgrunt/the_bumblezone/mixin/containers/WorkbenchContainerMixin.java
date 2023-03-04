72d7dfdd45f735b77c63e6aa7b2d0f871ec163f3
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-03-03 20:35:24 -0500

    fastworkbench incompat fix

--- src/main/java/com/telepathicgrunt/the_bumblezone/mixin/containers/WorkbenchContainerMixin.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/mixin/containers/WorkbenchContainerMixin.java
@@ -2,26 +2,35 @@ package com.telepathicgrunt.the_bumblezone.mixin.containers;
 
 import com.telepathicgrunt.the_bumblezone.modinit.BzCriterias;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.Container;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.CraftingMenu;
-import net.minecraft.world.item.crafting.CraftingRecipe;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.inventory.ResultContainer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CraftingMenu.class)
 public class WorkbenchContainerMixin {
 
+    @Shadow
+    @Final
+    private ResultContainer resultSlots;
+
+    @Shadow
+    @Final
+    private Player player;
+
     // makes RecipeDiscoveredTrigger work.
-    @ModifyVariable(method = "slotChangedCraftingGrid(Lnet/minecraft/world/inventory/AbstractContainerMenu;Lnet/minecraft/world/level/Level;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/inventory/CraftingContainer;Lnet/minecraft/world/inventory/ResultContainer;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/ResultContainer;setRecipeUsed(Lnet/minecraft/world/level/Level;Lnet/minecraft/server/level/ServerPlayer;Lnet/minecraft/world/item/crafting/Recipe;)Z"),
+    @Inject(method = "slotsChanged(Lnet/minecraft/world/Container;)V",
+            at = @At(value = "TAIL"),
             require = 0)
-    private static CraftingRecipe thebumblezone_recipeDiscoveredTrigger(CraftingRecipe recipe, AbstractContainerMenu handler, Level world, Player player) {
-        if (player instanceof ServerPlayer serverPlayer) {
-            BzCriterias.RECIPE_DISCOVERED_TRIGGER.trigger(serverPlayer, recipe.getId());
+    private void thebumblezone_recipeDiscoveredTrigger1(Container container, CallbackInfo ci) {
+        if (player instanceof ServerPlayer serverPlayer && this.resultSlots.getRecipeUsed() != null) {
+            BzCriterias.RECIPE_DISCOVERED_TRIGGER.trigger(serverPlayer, resultSlots.getRecipeUsed().getId());
         }
-        return recipe;
     }
 }
