81c619ec7eb6a65091e8a7bf5cb0bdfadfd39cc8
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-03-16 22:33:44 -0400

    fluid behavior changes
    https://github.com/TelepathicGrunt/Bumblezone/issues/220
    
    https://github.com/TelepathicGrunt/Bumblezone/issues/207

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,10 @@ Updated to 1.19.4
 
 ##### Items:
 Fixed Bumblebee Chestplate and Honeybee Leggings rendering improperly if more than one of them is rendering in the world.
+
+##### Fluids:
+
+Honey Fluid and Royal Jelly Fluid now turns into Glistering Honey Crystal block when it touches non-honey fluids!
+
+##### Blocks:
+Sugar Infused Stone and Sugar Infused Cobblestone will now turn neighboring Water tagged fluids into Sugar Water fluid.
--- common/src/main/java/com/telepathicgrunt/the_bumblezone/blocks/SugarInfusedStone.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/blocks/SugarInfusedStone.java
@@ -1,14 +1,56 @@
 package com.telepathicgrunt.the_bumblezone.blocks;
 
+import com.telepathicgrunt.the_bumblezone.modinit.BzBlocks;
+import com.telepathicgrunt.the_bumblezone.modinit.BzFluids;
+import com.telepathicgrunt.the_bumblezone.modinit.BzTags;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
+
+import static com.telepathicgrunt.the_bumblezone.fluids.HoneyFluidBlock.BOTTOM_LEVEL;
 
 
 public class SugarInfusedStone extends Block {
 
     public SugarInfusedStone() {
         super(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.STONE).requiresCorrectToolForDrops().strength(1.5F, 6.0F));
+    }
+
+    @Override
+    public void neighborChanged(BlockState blockState, Level level, BlockPos blockPos, Block block, BlockPos blockPos1, boolean b) {
+        if (!level.isClientSide()) {
+            sugarifyNeighboringWater(level, blockPos);
+        }
+
+        super.neighborChanged(blockState, level, blockPos, block, blockPos1, b);
+    }
+
+    @Override
+    public void setPlacedBy(Level level, BlockPos blockPos, BlockState state, LivingEntity placer, ItemStack stack) {
+        if (!level.isClientSide()) {
+            sugarifyNeighboringWater(level, blockPos);
+        }
+
+        super.setPlacedBy(level, blockPos, state, placer, stack);
+    }
+
+    private static void sugarifyNeighboringWater(LevelAccessor level, BlockPos blockPos) {
+        for (Direction direction : Direction.values()) {
+            BlockPos sidePos = blockPos.relative(direction);
+            FluidState sideFluid = level.getFluidState(sidePos);
+            if(sideFluid.is(BzTags.CONVERTIBLE_TO_SUGAR_WATER) && sideFluid.isSource() && level.getBlockState(sidePos).getShape(level, sidePos).isEmpty()) {
+                level.setBlock(sidePos, BzFluids.SUGAR_WATER_BLOCK.get().defaultBlockState(), 3);
+            }
+        }
     }
 }
