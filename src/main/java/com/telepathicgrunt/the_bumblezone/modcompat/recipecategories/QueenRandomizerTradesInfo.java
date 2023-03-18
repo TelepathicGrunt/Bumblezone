bc7a2e3876a84060f2414f213d9e50cda44cc249
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-03-18 11:08:12 -0400

    finalize the randomizer trade category

---
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/recipecategories/QueenRandomizerTradesInfo.java
@@ -0,0 +1,11 @@
+package com.telepathicgrunt.the_bumblezone.modcompat.recipecategories;
+
+import mezz.jei.api.recipe.category.extensions.IRecipeCategoryExtension;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.List;
+
+public record QueenRandomizerTradesInfo(ItemStack input, List<ItemStack> randomizes, int weight, int totalGroupWeight) implements IRecipeCategoryExtension {
+
+}
