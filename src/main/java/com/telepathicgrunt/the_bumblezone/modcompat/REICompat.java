dfc60a6bb11e05140235566be80d737b79833b67
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2023-03-18 12:42:24 -0600

    add REI compat for bee queen trades

--- src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/REICompat.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/REICompat.java
@@ -1,24 +1,39 @@
 package com.telepathicgrunt.the_bumblezone.modcompat;
 
 import com.telepathicgrunt.the_bumblezone.Bumblezone;
+import com.telepathicgrunt.the_bumblezone.entities.queentrades.QueensTradeManager;
+import com.telepathicgrunt.the_bumblezone.entities.queentrades.TradeEntryReducedObj;
 import com.telepathicgrunt.the_bumblezone.items.recipes.IncenseCandleRecipe;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.rei.QueenRandomizerTradesREICategory;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.rei.QueenTradesREICategory;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.rei.REIQueenRandomizerTradesInfo;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.rei.REIQueenTradesInfo;
 import com.telepathicgrunt.the_bumblezone.modinit.BzFluids;
 import com.telepathicgrunt.the_bumblezone.modinit.BzItems;
 import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
 import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
 import me.shedaniel.rei.api.common.util.EntryStacks;
 import me.shedaniel.rei.plugin.client.BuiltinClientPlugin;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.random.WeightedRandomList;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingRecipe;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.level.material.Fluid;
 
 import java.util.List;
+import java.util.Map;
 
 public class REICompat implements REIClientPlugin {
+
+    public static final CategoryIdentifier<REIQueenTradesInfo> QUEEN_TRADES = CategoryIdentifier.of(Bumblezone.MODID, "queen_trades");
+    public static final CategoryIdentifier<REIQueenRandomizerTradesInfo> QUEEN_RANDOMIZE_TRADES = CategoryIdentifier.of(Bumblezone.MODID, "queen_color_randomizer_trades");
 
     @Override
     public void registerDisplays(DisplayRegistry registry) {
@@ -33,6 +48,26 @@ public class REICompat implements REIClientPlugin {
 
         registry.getRecipeManager().byKey(new ResourceLocation(Bumblezone.MODID, "incense_candle"))
                 .ifPresent(recipe -> registerExtraRecipes(recipe, registry, false));
+
+        if (!QueensTradeManager.QUEENS_TRADE_MANAGER.tradeReduced.isEmpty()) {
+            for (Map.Entry<Item, WeightedRandomList<TradeEntryReducedObj>> trade : QueensTradeManager.QUEENS_TRADE_MANAGER.tradeReduced.entrySet()) {
+                for (TradeEntryReducedObj tradeResult : trade.getValue().unwrap()) {
+                    if (!tradeResult.randomizerTrade()) {
+                        registry.add(new REIQueenTradesInfo(List.of(EntryIngredients.of(trade.getKey())), List.of(EntryIngredients.of(tradeResult.item(), tradeResult.count())), tradeResult.xpReward(), tradeResult.weight(), tradeResult.totalGroupWeight()), QUEEN_TRADES);
+                    }
+                }
+            }
+        }
+
+        if (!QueensTradeManager.QUEENS_TRADE_MANAGER.tradeReduced.isEmpty()) {
+            for (List<TradeEntryReducedObj> tradeEntry : QueensTradeManager.QUEENS_TRADE_MANAGER.tradeRandomizer) {
+                List<ItemStack> randomizeStack = tradeEntry.stream().map(e -> e.item().getDefaultInstance()).toList();
+                for (ItemStack input : randomizeStack) {
+                    registry.add(new REIQueenRandomizerTradesInfo(List.of(EntryIngredients.of(input)), List.of(EntryIngredients.ofItemStacks(randomizeStack)), 1, randomizeStack.size()), QUEEN_RANDOMIZE_TRADES);
+                }
+            }
+        }
+
     }
 
     private static void addInfo(Item item) {
@@ -60,5 +95,15 @@ public class REICompat implements REIClientPlugin {
             List<CraftingRecipe> extraRecipes = FakeIncenseCandleRecipeCreator.constructFakeRecipes(incenseCandleRecipe, oneRecipeOnly);
             extraRecipes.forEach(registry::add);
         }
+    }
+}
+
+    @Override
+    public void registerCategories(CategoryRegistry registry) {
+        registry.add(new QueenTradesREICategory());
+        registry.add(new QueenRandomizerTradesREICategory());
+
+        registry.addWorkstations(QUEEN_TRADES, EntryStacks.of(BzItems.BEE_QUEEN_SPAWN_EGG.get()));
+        registry.addWorkstations(QUEEN_RANDOMIZE_TRADES, EntryStacks.of(BzItems.BEE_QUEEN_SPAWN_EGG.get()));
     }
 }
---
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/recipecategories/rei/QueenRandomizerTradesREICategory.java
@@ -0,0 +1,77 @@
+package com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.rei;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.telepathicgrunt.the_bumblezone.Bumblezone;
+import com.telepathicgrunt.the_bumblezone.modcompat.REICompat;
+import com.telepathicgrunt.the_bumblezone.modinit.BzItems;
+import me.shedaniel.math.Point;
+import me.shedaniel.math.Rectangle;
+import me.shedaniel.rei.api.client.gui.AbstractRenderer;
+import me.shedaniel.rei.api.client.gui.Renderer;
+import me.shedaniel.rei.api.client.gui.widgets.Widget;
+import me.shedaniel.rei.api.client.gui.widgets.Widgets;
+import me.shedaniel.rei.api.client.registry.display.DisplayCategory;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.util.EntryStacks;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QueenRandomizerTradesREICategory implements DisplayCategory<REIQueenRandomizerTradesInfo> {
+
+	public static final int RECIPE_WIDTH = 136;
+	public static final int RECIPE_HEIGHT = 28;
+
+	private final Renderer icon;
+	private final Component localizedName;
+
+	public QueenRandomizerTradesREICategory() {
+		this.icon = new QueenEggIconRenderer(new ResourceLocation(Bumblezone.MODID, "textures/gui/bee_queen_randomize_trades.png"));
+		this.localizedName = Component.translatable("the_bumblezone.jei.bee_queen_color_randomizing_trades");
+	}
+
+	@Override
+	public CategoryIdentifier<? extends REIQueenRandomizerTradesInfo> getCategoryIdentifier() {
+		return REICompat.QUEEN_RANDOMIZE_TRADES;
+	}
+
+	@Override
+	public Component getTitle() {
+		return this.localizedName;
+	}
+
+	@Override
+	public Renderer getIcon() {
+		return this.icon;
+	}
+
+	@Override
+	public int getDisplayWidth(REIQueenRandomizerTradesInfo display) {
+		return RECIPE_WIDTH + 8;
+	}
+
+	@Override
+	public int getDisplayHeight() {
+		return RECIPE_HEIGHT + 8;
+	}
+
+	@Override
+	public List<Widget> setupDisplay(REIQueenRandomizerTradesInfo display, Rectangle origin) {
+		List<Widget> widgets = new ArrayList<>();
+		widgets.add(Widgets.createRecipeBase(origin));
+		Rectangle bounds = origin.getBounds();
+		bounds.translate(4, 4);
+
+		widgets.add(Widgets.createTexturedWidget(new ResourceLocation(Bumblezone.MODID, "textures/gui/queen_randomizer_trades_jei.png"), new Rectangle(bounds.getX(), bounds.getY(), RECIPE_WIDTH, RECIPE_HEIGHT)));
+		widgets.add(Widgets.createSlot(new Rectangle(bounds.getX() + 5, bounds.getY() + 5, 18, 18)).entries(display.getInputEntries().get(0)).markInput().disableBackground());
+		widgets.add(Widgets.createSlot(new Rectangle(bounds.getX() + 63, bounds.getY() + 5, 18, 18)).entries(display.getOutputEntries().get(0)).markOutput().disableBackground());
+
+		widgets.add(Widgets.createLabel(new Point(bounds.getX() + 86, bounds.getY() + 10), Component.translatable("the_bumblezone.jei.queen_trade_colors", display.getOutputEntries().get(0).size())).leftAligned().noShadow().color(0xFF404040, 0xFFBBBBBB));
+
+		String percent = String.valueOf((double)(display.getWeight()) / (display.getGroupWeight()) * 100);
+		widgets.add(Widgets.createTooltip(new Rectangle(bounds.getX() + 32, bounds.getY() + 6, 22, 16), Component.translatable("the_bumblezone.jei.queen_trade_chance", percent.substring(0, Math.min(percent.length(), 5)))));
+		return widgets;
+	}
+}
