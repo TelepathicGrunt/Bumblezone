ca93d606d7c467ba6cd12f26ee67c38afbf831e6
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-03-19 08:24:17 -0400

    improved rei/jei/emi layout

--- src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/recipecategories/emi/EMIQueenRandomizerTradesInfo.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/recipecategories/emi/EMIQueenRandomizerTradesInfo.java
@@ -1,15 +1,19 @@
 package com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.emi;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import com.telepathicgrunt.the_bumblezone.Bumblezone;
 import com.telepathicgrunt.the_bumblezone.modcompat.EMICompat;
 import dev.emi.emi.api.recipe.EmiRecipe;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
+import dev.emi.emi.api.widget.DrawableWidget;
 import dev.emi.emi.api.widget.GeneratedSlotWidget;
+import dev.emi.emi.api.widget.SlotWidget;
 import dev.emi.emi.api.widget.TextWidget;
 import dev.emi.emi.api.widget.TextureWidget;
 import dev.emi.emi.api.widget.WidgetHolder;
+import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
@@ -18,15 +22,17 @@ import java.util.List;
 
 public class EMIQueenRandomizerTradesInfo implements EmiRecipe {
 
-	private final List<EmiIngredient> inputs;
+	private final EmiIngredient input;
 	private final List<EmiStack> outputs;
+	private final EmiIngredient visualOutputs;
 	private final int weight;
 	private final int groupWeight;
 
-	public EMIQueenRandomizerTradesInfo(List<EmiIngredient> inputs, List<EmiStack> outputs, int weight, int groupWeight) {
+	public EMIQueenRandomizerTradesInfo(EmiIngredient input, List<EmiStack> outputs, int weight, int groupWeight) {
 		super();
-		this.inputs = inputs;
+		this.input = input;
 		this.outputs = outputs;
+		this.visualOutputs = EmiIngredient.of(outputs);
 		this.weight = weight;
 		this.groupWeight = groupWeight;
 	}
@@ -51,7 +57,7 @@ public class EMIQueenRandomizerTradesInfo implements EmiRecipe {
 
 	@Override
 	public List<EmiIngredient> getInputs() {
-		return inputs;
+		return List.of(input);
 	}
 
 	@Override
@@ -73,20 +79,19 @@ public class EMIQueenRandomizerTradesInfo implements EmiRecipe {
 	public void addWidgets(WidgetHolder widgets) {
 		widgets.add(new TextureWidget(new ResourceLocation(Bumblezone.MODID, "textures/gui/queen_randomizer_trades_jei.png"), 0, 0, getDisplayWidth(), getDisplayHeight(), 0, 0));
 
-		widgets.add(new GeneratedSlotWidget((random) ->  {
-			long nowInSeconds = System.currentTimeMillis() / 1000;
-			return inputs.get((int) (nowInSeconds % inputs.size()));
-		}, 1001, 5, 5));
-
-		widgets.add(new GeneratedSlotWidget((random) ->  {
-			long nowInSeconds = System.currentTimeMillis() / 1000;
-			return outputs.get((int) (nowInSeconds % outputs.size()));
-		}, 3444, 63, 5));
-
+		widgets.add(new SlotWidget(input, 5, 5));
+		widgets.add(new SlotWidget(visualOutputs, 63, 5));
 
 		widgets.add(new TextWidget(Component.translatable("the_bumblezone.jei.queen_trade_colors", getOutputs().size()).getVisualOrderText(), 86,  10, 0xFF404040, false));
 
-		String percent = String.valueOf((double)(getWeight()) / (getGroupWeight()) * 100);
-		//widgets.add(Widgets.createTooltip(new Rectangle(bounds.getX() + 32, bounds.getY() + 6, 22, 16), Component.translatable("the_bumblezone.jei.queen_trade_chance", percent.substring(0, Math.min(percent.length(), 5)))));
+		double percentValue = ((double)(getWeight()) / getGroupWeight()) * 100D;
+		String percent = String.valueOf(percentValue);
+		String percentRounded = String.valueOf(Math.max(Math.round(percentValue), 1));
+
+		DrawableWidget tooltipWidget = new DrawableWidget(32, 6, 22, 16, (matrices, mouseX, mouseY, delta) -> {});
+		tooltipWidget.tooltip((x, z) -> List.of(ClientTooltipComponent.create(Component.translatable("the_bumblezone.jei.queen_trade_chance_tooltip", percent.substring(0, Math.min(percent.length(), 5))).getVisualOrderText())));
+		widgets.add(tooltipWidget);
+
+		widgets.add(new TextWidget(Component.translatable("the_bumblezone.jei.queen_trade_chance_text", percentRounded).getVisualOrderText(), 38 - (percentRounded.length() * 3), 11, 0xFF404040, false));
 	}
 }
--- src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/recipecategories/jei/DrawableAnimated.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/recipecategories/jei/DrawableAnimated.java
@@ -1,48 +1,14 @@
 package com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.jei;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import mezz.jei.api.gui.ITickTimer;
 import mezz.jei.api.gui.drawable.IDrawableAnimated;
 import mezz.jei.api.gui.drawable.IDrawableStatic;
-import mezz.jei.common.util.TickTimer;
 
 public class DrawableAnimated implements IDrawableAnimated {
     private final IDrawableStatic drawable;
-    private final ITickTimer tickTimer;
-    private final StartDirection startDirection;
 
-    private static StartDirection invert(StartDirection startDirection) {
-        return switch (startDirection) {
-            case TOP -> StartDirection.BOTTOM;
-            case BOTTOM -> StartDirection.TOP;
-            case LEFT -> StartDirection.RIGHT;
-            case RIGHT -> StartDirection.LEFT;
-        };
-    }
-
-    public DrawableAnimated(IDrawableStatic drawable, int ticksPerCycle, StartDirection startDirection, boolean inverted) {
-        final StartDirection animationStartDirection;
-        if (inverted) {
-            animationStartDirection = invert(startDirection);
-        } else {
-            animationStartDirection = startDirection;
-        }
-
-        int tickTimerMaxValue;
-        if (animationStartDirection == StartDirection.TOP || animationStartDirection == StartDirection.BOTTOM) {
-            tickTimerMaxValue = drawable.getHeight();
-        } else {
-            tickTimerMaxValue = drawable.getWidth();
-        }
+    public DrawableAnimated(IDrawableStatic drawable) {
         this.drawable = drawable;
-        this.tickTimer = new TickTimer(ticksPerCycle, tickTimerMaxValue, !inverted);
-        this.startDirection = animationStartDirection;
-    }
-
-    public DrawableAnimated(IDrawableStatic drawable, ITickTimer tickTimer, StartDirection startDirection) {
-        this.drawable = drawable;
-        this.tickTimer = tickTimer;
-        this.startDirection = startDirection;
     }
 
     @Override
@@ -61,16 +27,6 @@ public class DrawableAnimated implements IDrawableAnimated {
         int maskRight = 0;
         int maskTop = 0;
         int maskBottom = 0;
-
-        int animationValue = tickTimer.getValue();
-
-        switch (startDirection) {
-            case TOP -> maskBottom = animationValue;
-            case BOTTOM -> maskTop = animationValue;
-            case LEFT -> maskRight = animationValue;
-            case RIGHT -> maskLeft = animationValue;
-            default -> throw new IllegalStateException("Unknown startDirection " + startDirection);
-        }
 
         drawable.draw(poseStack, xOffset, yOffset, maskTop, maskBottom, maskLeft, maskRight);
     }
