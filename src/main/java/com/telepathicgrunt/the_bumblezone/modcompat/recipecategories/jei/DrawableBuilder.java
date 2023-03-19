9b336ec64f550b6b43c46659d298dab38eaee86a
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-03-18 22:47:49 -0400

    fixed jei possible issue

---
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/recipecategories/jei/DrawableBuilder.java
@@ -0,0 +1,73 @@
+package com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.jei;
+
+import mezz.jei.api.gui.ITickTimer;
+import mezz.jei.api.gui.drawable.IDrawableAnimated;
+import mezz.jei.api.gui.drawable.IDrawableBuilder;
+import mezz.jei.api.gui.drawable.IDrawableStatic;
+import net.minecraft.resources.ResourceLocation;
+
+public class DrawableBuilder implements IDrawableBuilder {
+    private final ResourceLocation resourceLocation;
+    private int u;
+    private int v;
+    private int width;
+    private int height;
+    private int textureWidth = 256;
+    private int textureHeight = 256;
+    private int paddingTop = 0;
+    private int paddingBottom = 0;
+    private int paddingLeft = 0;
+    private int paddingRight = 0;
+
+    public DrawableBuilder(ResourceLocation resourceLocation, int u, int v, int width, int height) {
+        assert resourceLocation != null;
+        this.u = u;
+        this.v = v;
+        this.width = width;
+        this.height = height;
+        this.resourceLocation = resourceLocation;
+    }
+
+    @Override
+    public IDrawableBuilder setTextureSize(int width, int height) {
+        this.textureWidth = width;
+        this.textureHeight = height;
+        return this;
+    }
+
+    @Override
+    public IDrawableBuilder addPadding(int paddingTop, int paddingBottom, int paddingLeft, int paddingRight) {
+        this.paddingTop = paddingTop;
+        this.paddingBottom = paddingBottom;
+        this.paddingLeft = paddingLeft;
+        this.paddingRight = paddingRight;
+        return this;
+    }
+
+    @Override
+    public IDrawableBuilder trim(int trimTop, int trimBottom, int trimLeft, int trimRight) {
+        this.u += trimLeft;
+        this.v += trimTop;
+        this.width -= trimLeft + trimRight;
+        this.height -= trimTop + trimBottom;
+        return this;
+    }
+
+    @Override
+    public IDrawableStatic build() {
+        return new DrawableResource(resourceLocation, u, v, width, height, paddingTop, paddingBottom, paddingLeft, paddingRight, textureWidth, textureHeight);
+    }
+
+    @Override
+    public IDrawableAnimated buildAnimated(int ticksPerCycle, IDrawableAnimated.StartDirection startDirection, boolean inverted) {
+        IDrawableStatic drawable = build();
+        return new DrawableAnimated(drawable, ticksPerCycle, startDirection, inverted);
+    }
+
+    @Override
+    public IDrawableAnimated buildAnimated(ITickTimer tickTimer, IDrawableAnimated.StartDirection startDirection) {
+        IDrawableStatic drawable = build();
+        return new DrawableAnimated(drawable, tickTimer, startDirection);
+    }
+}
+
--- src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/recipecategories/jei/QueenTradesJEICategory.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/recipecategories/jei/QueenTradesJEICategory.java
@@ -12,7 +12,6 @@ import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.recipe.category.IRecipeCategory;
-import mezz.jei.library.gui.elements.DrawableBuilder;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
