9cbce7712c0c16bad871e60ed06be14741edf24d
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-03-18 23:36:02 -0400

    partial emi progress

--- src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/EMICompat.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/EMICompat.java
@@ -1,18 +1,33 @@
 package com.telepathicgrunt.the_bumblezone.modcompat;
 
 import com.telepathicgrunt.the_bumblezone.Bumblezone;
+import com.telepathicgrunt.the_bumblezone.entities.queentrades.QueensTradeManager;
+import com.telepathicgrunt.the_bumblezone.entities.queentrades.TradeEntryReducedObj;
 import com.telepathicgrunt.the_bumblezone.items.recipes.IncenseCandleRecipe;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.emi.EMIQueenRandomizerTradesInfo;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.emi.EMIQueenTradesInfo;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.emi.QueenRandomizerTradesEMICategory;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.emi.QueenTradesEMICategory;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.rei.QueenRandomizerTradesREICategory;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.rei.QueenTradesREICategory;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.rei.REIQueenRandomizerTradesInfo;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.rei.REIQueenTradesInfo;
 import com.telepathicgrunt.the_bumblezone.modinit.BzFluids;
 import com.telepathicgrunt.the_bumblezone.modinit.BzItems;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.EmiCraftingRecipe;
 import dev.emi.emi.api.recipe.EmiInfoRecipe;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.render.EmiTexture;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
 import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.random.WeightedRandomList;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingRecipe;
@@ -21,8 +36,13 @@ import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.level.material.Fluid;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class EMICompat implements EmiPlugin {
+    public static final EmiStack WORKSTATION = EmiStack.of(BzItems.BEE_QUEEN_SPAWN_EGG);
+    public static final EmiRecipeCategory QUEEN_TRADES = new QueenTradesEMICategory();
+    public static final EmiRecipeCategory QUEEN_RANDOMIZE_TRADES = new QueenRandomizerTradesEMICategory();
 
     @Override
     public void register(EmiRegistry registry) {
@@ -36,6 +56,32 @@ public class EMICompat implements EmiPlugin {
                 .ifPresent(recipe -> registerExtraRecipes(recipe, registry, true));
         registry.getRecipeManager().byKey(new ResourceLocation(Bumblezone.MODID, "incense_candle"))
                 .ifPresent(recipe -> registerExtraRecipes(recipe, registry, false));
+
+        registry.addCategory(QUEEN_TRADES);
+        registry.addCategory(QUEEN_RANDOMIZE_TRADES);
+
+        registry.addWorkstation(QUEEN_TRADES, EmiIngredient.of(Ingredient.of(BzItems.BEE_QUEEN_SPAWN_EGG)));
+        registry.addWorkstation(QUEEN_RANDOMIZE_TRADES, EmiIngredient.of(Ingredient.of(BzItems.BEE_QUEEN_SPAWN_EGG)));
+
+        if (!QueensTradeManager.QUEENS_TRADE_MANAGER.tradeReduced.isEmpty()) {
+            for (Map.Entry<Item, WeightedRandomList<TradeEntryReducedObj>> trade : QueensTradeManager.QUEENS_TRADE_MANAGER.tradeReduced.entrySet()) {
+                for (TradeEntryReducedObj tradeResult : trade.getValue().unwrap()) {
+                    if (!tradeResult.randomizerTrade()) {
+                        List<EmiStack> rewardCollection = tradeResult.items().stream().map(e -> EmiStack.of(new ItemStack(e, tradeResult.count()))).toList();
+                        registry.addRecipe(new EMIQueenTradesInfo(List.of(EmiIngredient.of(Ingredient.of(trade.getKey()))), rewardCollection, tradeResult.xpReward(), tradeResult.weight(), tradeResult.totalGroupWeight()));
+                    }
+                }
+            }
+        }
+
+        if (!QueensTradeManager.QUEENS_TRADE_MANAGER.tradeReduced.isEmpty()) {
+            for (TradeEntryReducedObj tradeEntry : QueensTradeManager.QUEENS_TRADE_MANAGER.tradeRandomizer) {
+                List<ItemStack> randomizeStack = tradeEntry.items().stream().map(Item::getDefaultInstance).toList();
+                for (ItemStack input : randomizeStack) {
+                    registry.addRecipe(new REIQueenRandomizerTradesInfo(List.of(EmiIngredient.of(Ingredient.of(input))), randomizeStack.stream().map(EmiStack::of).collect(Collectors.toList()), 1, randomizeStack.size()));
+                }
+            }
+        }
     }
 
     private static void registerExtraRecipes(Recipe<?> baseRecipe, EmiRegistry registry, boolean oneRecipeOnly) {
