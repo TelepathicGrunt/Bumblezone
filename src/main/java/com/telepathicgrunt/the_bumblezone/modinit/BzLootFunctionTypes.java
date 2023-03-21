7cf1647549d42034d6078fa0680e46466ec9822d
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-03-20 22:24:02 -0400

    Balance cocoon loot

--- src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzLootFunctionTypes.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzLootFunctionTypes.java
@@ -4,6 +4,7 @@ import com.telepathicgrunt.the_bumblezone.Bumblezone;
 import com.telepathicgrunt.the_bumblezone.items.functions.DropContainerItems;
 import com.telepathicgrunt.the_bumblezone.items.functions.HoneyCompassLocateStructure;
 import com.telepathicgrunt.the_bumblezone.items.functions.PrefillMap;
+import com.telepathicgrunt.the_bumblezone.items.functions.TagItemRemovals;
 import com.telepathicgrunt.the_bumblezone.items.functions.UniquifyIfHasItems;
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.storage.loot.functions.LootItemFunctionType;
@@ -18,4 +19,5 @@ public class BzLootFunctionTypes {
     public static final RegistryObject<LootItemFunctionType> UNIQUIFY_IF_HAS_ITEMS = LOOT_ITEM_FUNCTION_TYPE.register("uniquify_if_has_items", () -> new LootItemFunctionType(new UniquifyIfHasItems.Serializer()));
     public static final RegistryObject<LootItemFunctionType> HONEY_COMPASS_LOCATE_STRUCTURE = LOOT_ITEM_FUNCTION_TYPE.register("honey_compass_locate_structure", () -> new LootItemFunctionType(new HoneyCompassLocateStructure.Serializer()));
     public static final RegistryObject<LootItemFunctionType> PREFILL_MAP = LOOT_ITEM_FUNCTION_TYPE.register("prefill_map", () -> new LootItemFunctionType(new PrefillMap.Serializer()));
+    public static final RegistryObject<LootItemFunctionType> TAG_ITEM_REMOVALS = LOOT_ITEM_FUNCTION_TYPE.register("tag_item_removals", () -> new LootItemFunctionType(new TagItemRemovals.Serializer()));
 }
