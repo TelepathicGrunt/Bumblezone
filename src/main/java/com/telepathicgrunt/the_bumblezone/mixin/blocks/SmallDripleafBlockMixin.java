d99f4c35886a121a8c3441c6f413b8ab3f489052
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-03-26 14:54:40 -0400

    waterlogging mixins

---
+++ src/main/java/com/telepathicgrunt/the_bumblezone/mixin/blocks/SmallDripleafBlockMixin.java
@@ -0,0 +1,30 @@
+package com.telepathicgrunt.the_bumblezone.mixin.blocks;
+
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.telepathicgrunt.the_bumblezone.modinit.BzFluids;
+import com.telepathicgrunt.the_bumblezone.modinit.BzTags;
+import net.minecraft.world.level.block.BigDripleafBlock;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SmallDripleafBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import net.minecraft.world.level.material.Fluids;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(SmallDripleafBlock.class)
+public class SmallDripleafBlockMixin {
+
+    @ModifyExpressionValue(method = "mayPlaceOn(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;)Z",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/BlockGetter;getFluidState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/material/FluidState;"),
+            require = 0)
+    private static FluidState thebumblezone_waterlogWhenPlacedIntoSugarWater(FluidState fluid) {
+        if(fluid.is(BzTags.SUGAR_WATER_FLUID) && Blocks.BIG_DRIPLEAF.defaultBlockState().is(BzTags.WATERLOGGABLE_BLOCKS_WHEN_PLACED_IN_FLUID)) {
+            return Fluids.WATER.defaultFluidState();
+        }
+        return fluid;
+    }
+}
