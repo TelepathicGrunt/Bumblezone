3fcfd03746fcfb0f3ca5a81fd10dd7d7fbb5edd1
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-03-26 15:10:57 -0400

    undo beehemoth keybind hack removal

--- src/main/java/com/telepathicgrunt/the_bumblezone/client/BeehemothControls.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/client/BeehemothControls.java
@@ -1,7 +1,9 @@
 package com.telepathicgrunt.the_bumblezone.client;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import com.telepathicgrunt.the_bumblezone.Bumblezone;
 import com.telepathicgrunt.the_bumblezone.entities.mobs.BeehemothEntity;
+import com.telepathicgrunt.the_bumblezone.mixin.client.KeyMappingAccessor;
 import com.telepathicgrunt.the_bumblezone.packets.BeehemothControlsPacket;
 import io.netty.buffer.Unpooled;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
@@ -11,8 +13,8 @@ import net.minecraft.network.FriendlyByteBuf;
 import org.lwjgl.glfw.GLFW;
 
 public class BeehemothControls {
-    public static final KeyMapping KEY_BIND_BEEHEMOTH_DOWN = new KeyMapping("key." + Bumblezone.MODID + ".beehemoth_down", GLFW.GLFW_KEY_CAPS_LOCK, "key.categories." + Bumblezone.MODID);
-    public static final KeyMapping KEY_BIND_BEEHEMOTH_UP = new KeyMapping("key." + Bumblezone.MODID + ".beehemoth_up", GLFW.GLFW_KEY_SPACE, "key.categories." + Bumblezone.MODID);
+    public static final KeyMapping KEY_BIND_BEEHEMOTH_DOWN = createSafeKeyMapping("key." + Bumblezone.MODID + ".beehemoth_down", GLFW.GLFW_KEY_CAPS_LOCK);
+    public static final KeyMapping KEY_BIND_BEEHEMOTH_UP = createSafeKeyMapping("key." + Bumblezone.MODID + ".beehemoth_up", GLFW.GLFW_KEY_SPACE);
 
     public static void keyInput(int key, int scancode, int action) {
         if (Minecraft.getInstance().player != null &&
@@ -35,5 +37,14 @@ public class BeehemothControls {
                 }
             }
         }
+    }
+
+    private static KeyMapping createSafeKeyMapping(String description, int keycode) {
+        InputConstants.Key key = InputConstants.Type.KEYSYM.getOrCreate(keycode);
+        KeyMapping oldMapping = KeyMappingAccessor.getMAP().get(key);
+        KeyMapping keyMapping = new KeyMapping(description, keycode, "key.categories." + Bumblezone.MODID);
+        KeyMappingAccessor.getMAP().put(key, oldMapping);
+        KeyMappingAccessor.getALL().remove(description);
+        return keyMapping;
     }
 }
