eae75eac8b029b1fe802953b9d5dc3b92f38c3a1
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-03-28 18:55:45 -0400

    added more tags for fine tuning waterlogging

--- src/main/java/com/telepathicgrunt/the_bumblezone/mixin/blocks/BigDripleafStemBlockMixin.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/mixin/blocks/BigDripleafStemBlockMixin.java
@@ -5,6 +5,7 @@ import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.telepathicgrunt.the_bumblezone.modinit.BzFluids;
 import com.telepathicgrunt.the_bumblezone.modinit.BzTags;
+import com.telepathicgrunt.the_bumblezone.utils.GeneralUtils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.BigDripleafBlock;
@@ -24,7 +25,7 @@ public class BigDripleafStemBlockMixin {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/material/FluidState;isSourceOfType(Lnet/minecraft/world/level/material/Fluid;)Z"),
             require = 0)
     private static boolean thebumblezone_waterlogWhenPlacedIntoSugarWater(boolean isWater, LevelAccessor levelAccessor, BlockPos blockPos, FluidState fluidState) {
-        if(fluidState.is(BzTags.SUGAR_WATER_FLUID) && Blocks.BIG_DRIPLEAF.defaultBlockState().is(BzTags.WATERLOGGABLE_BLOCKS_WHEN_PLACED_IN_FLUID)) {
+        if(fluidState.is(BzTags.SUGAR_WATER_FLUID) && GeneralUtils.isBlockAllowedForSugarWaterWaterlogging(Blocks.BIG_DRIPLEAF.defaultBlockState())) {
             return true;
         }
         return isWater;
