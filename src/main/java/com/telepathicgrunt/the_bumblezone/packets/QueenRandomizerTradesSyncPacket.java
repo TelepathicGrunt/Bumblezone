230ada5734d98999dac83bb34d8fe261bbb68f1a
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-04-27 20:56:44 -0400

    optimized packet and fixed tags

--- src/main/java/com/telepathicgrunt/the_bumblezone/packets/QueenRandomizerTradesSyncPacket.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/packets/QueenRandomizerTradesSyncPacket.java
@@ -5,23 +5,33 @@ import com.google.gson.GsonBuilder;
 import com.mojang.serialization.DataResult;
 import com.telepathicgrunt.the_bumblezone.Bumblezone;
 import com.telepathicgrunt.the_bumblezone.entities.queentrades.QueensTradeManager;
+import com.telepathicgrunt.the_bumblezone.modcompat.recipecategories.RandomizeTradeRowInput;
+import dev.architectury.registry.registries.Registries;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.StringTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.network.PacketDistributor;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Supplier;
 
-public record QueenRandomizerTradesSyncPacket(List<QueensTradeManager.TradeWantEntry> recipeViewerRandomizerTrades) {
+public record QueenRandomizerTradesSyncPacket(List<RandomizeTradeRowInput> recipeViewerRandomizerTrades) {
     public static Gson gson = new GsonBuilder().create();
 
-    public static void sendToClient(ServerPlayer entity, List<QueensTradeManager.TradeWantEntry> recipeViewerRandomizerTrades) {
+    public static void sendToClient(ServerPlayer entity, List<RandomizeTradeRowInput> recipeViewerRandomizerTrades) {
         MessageHandler.DEFAULT_CHANNEL.send(PacketDistributor.PLAYER.with(() -> entity),
                 new QueenRandomizerTradesSyncPacket(recipeViewerRandomizerTrades));
     }
@@ -30,7 +40,7 @@ public record QueenRandomizerTradesSyncPacket(List<QueensTradeManager.TradeWantE
      * How the client will read the packet.
      */
     public static QueenRandomizerTradesSyncPacket parse(final FriendlyByteBuf buf) {
-        List<QueensTradeManager.TradeWantEntry> parsedData = new ArrayList<>();
+        List<RandomizeTradeRowInput> parsedData = new ArrayList<>();
 
         CompoundTag data = buf.readAnySizeNbt();
         if (data == null) {
@@ -38,12 +48,11 @@ public record QueenRandomizerTradesSyncPacket(List<QueensTradeManager.TradeWantE
             return new QueenRandomizerTradesSyncPacket(parsedData);
         }
 
-        ListTag tagList = data.getList("randomize_trades", Tag.TAG_COMPOUND);
+        ListTag tagList = data.getList("randomize_trades", Tag.TAG_STRING);
         for (int i = 0; i < tagList.size(); i++) {
-            CompoundTag tradeCompound = tagList.getCompound(i);
-            DataResult<QueensTradeManager.TradeWantEntry> dataResult = QueensTradeManager.TradeWantEntry.CODEC.parse(NbtOps.INSTANCE, tradeCompound);
-            dataResult.error().ifPresent(e -> Bumblezone.LOGGER.error("Failed to parse Queen Randomizer Trade packet entry: {}", e.toString()));
-            dataResult.result().ifPresent(parsedData::add);
+            TagKey<Item> tagKey = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(tagList.getString(i)));
+            RandomizeTradeRowInput wantEntry = new RandomizeTradeRowInput(Optional.of(tagKey));
+            parsedData.add(wantEntry);
         }
 
         return new QueenRandomizerTradesSyncPacket(parsedData);
@@ -56,9 +65,7 @@ public record QueenRandomizerTradesSyncPacket(List<QueensTradeManager.TradeWantE
         CompoundTag data = new CompoundTag();
         ListTag listTag = new ListTag();
         for (int i = 0; i < pkt.recipeViewerRandomizerTrades().size(); i++) {
-            DataResult<Tag> dataResult = QueensTradeManager.TradeWantEntry.CODEC.encodeStart(NbtOps.INSTANCE, pkt.recipeViewerRandomizerTrades().get(i));
-            dataResult.error().ifPresent(e -> Bumblezone.LOGGER.error("Failed to encode Queen Randomizer Trade packet entry: {}", e.toString()));
-            dataResult.result().ifPresent(listTag::add);
+            listTag.add(StringTag.valueOf(pkt.recipeViewerRandomizerTrades().get(i).tagKey().get().location().toString()));
         }
         data.put("randomize_trades", listTag);
         buf.writeNbt(data);
