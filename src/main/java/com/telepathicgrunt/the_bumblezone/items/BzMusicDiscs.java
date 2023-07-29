cda551f37dbfe2a3abe7519100b04429ae23f77e
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-07-28 22:14:48 -0400

    fixed wrapping issues

--- src/main/java/com/telepathicgrunt/the_bumblezone/items/BzMusicDiscs.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/items/BzMusicDiscs.java
@@ -24,7 +24,8 @@ public class BzMusicDiscs extends RecordItem {
     public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> list, TooltipFlag tooltipFlag) {
         list.add(this.getDisplayName().withStyle(ChatFormatting.GRAY));
         if (this.hasDownload) {
-            list.add(Component.translatable(this.getDescriptionId() + ".download").withStyle(tooltipFlag.isAdvanced() ? ChatFormatting.DARK_PURPLE : ChatFormatting.DARK_GRAY));
+            list.add(Component.translatable(this.getDescriptionId() + ".download1").withStyle(tooltipFlag.isAdvanced() ? ChatFormatting.DARK_PURPLE : ChatFormatting.DARK_GRAY));
+            list.add(Component.translatable(this.getDescriptionId() + ".download2").withStyle(tooltipFlag.isAdvanced() ? ChatFormatting.DARK_PURPLE : ChatFormatting.DARK_GRAY));
         }
     }
 }
