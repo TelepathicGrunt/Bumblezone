a738e752422d2640e17bae3c39ac14c7f4fab9b4
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-07-28 22:09:41 -0400

    life is pain and bugfixes abound

--- src/main/java/com/telepathicgrunt/the_bumblezone/items/BzMusicDiscs.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/items/BzMusicDiscs.java
@@ -1,11 +1,30 @@
 package com.telepathicgrunt.the_bumblezone.items;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.RecordItem;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class BzMusicDiscs extends RecordItem {
-    public BzMusicDiscs(int comparatorOutput, SoundEvent sound, Item.Properties settings, int musicTimeLength) {
+    private final boolean hasDownload;
+
+    public BzMusicDiscs(int comparatorOutput, SoundEvent sound, Item.Properties settings, int musicTimeLength, boolean hasDownload) {
         super(comparatorOutput, sound, settings, musicTimeLength);
+        this.hasDownload = hasDownload;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> list, TooltipFlag tooltipFlag) {
+        list.add(this.getDisplayName().withStyle(ChatFormatting.GRAY));
+        if (this.hasDownload) {
+            list.add(Component.translatable(this.getDescriptionId() + ".download").withStyle(tooltipFlag.isAdvanced() ? ChatFormatting.DARK_PURPLE : ChatFormatting.DARK_GRAY));
+        }
     }
 }
