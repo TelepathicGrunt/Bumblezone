31af6409fb7be673e174ff0edd05c7da42e44d3c
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-11-24 17:17:41 -0500

    fixed smelting recipes

---
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/items/recipes/ItemStackSmeltingRecipe.java
@@ -0,0 +1,140 @@
+package com.telepathicgrunt.the_bumblezone.items.recipes;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.telepathicgrunt.the_bumblezone.modinit.BzRecipes;
+import net.minecraft.core.NonNullList;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.util.ExtraCodecs;
+import net.minecraft.world.Container;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.crafting.CookingBookCategory;
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.SimpleCookingSerializer;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+
+public class ItemStackSmeltingRecipe extends AbstractCookingRecipe {
+    protected final RecipeType<?> type;
+    protected final CookingBookCategory category;
+    protected final String group;
+    protected final Ingredient ingredient;
+    protected final ItemStack result;
+    protected final float experience;
+    protected final int cookingTime;
+
+    public ItemStackSmeltingRecipe(String string, CookingBookCategory cookingBookCategory, Ingredient ingredient, ItemStack itemStack, float f, int i) {
+        super(RecipeType.SMELTING, string, cookingBookCategory, ingredient, itemStack, f, i);
+        this.type = RecipeType.SMELTING;
+        this.category = cookingBookCategory;
+        this.group = string;
+        this.ingredient = ingredient;
+        this.result = itemStack;
+        this.experience = f;
+        this.cookingTime = i;
+    }
+
+    @Override
+    public boolean matches(Container container, Level level) {
+        return this.ingredient.test(container.getItem(0));
+    }
+
+    @Override
+    public ItemStack assemble(Container container, RegistryAccess registryAccess) {
+        return this.result.copy();
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int i, int j) {
+        return true;
+    }
+
+    @Override
+    public NonNullList<Ingredient> getIngredients() {
+        NonNullList<Ingredient> nonNullList = NonNullList.create();
+        nonNullList.add(this.ingredient);
+        return nonNullList;
+    }
+
+    public float getExperience() {
+        return this.experience;
+    }
+
+    @Override
+    public ItemStack getResultItem(RegistryAccess registryAccess) {
+        return this.result;
+    }
+
+    @Override
+    public String getGroup() {
+        return this.group;
+    }
+
+    public int getCookingTime() {
+        return this.cookingTime;
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return this.type;
+    }
+
+    public CookingBookCategory category() {
+        return this.category;
+    }
+
+    @Override
+    public ItemStack getToastSymbol() {
+        return new ItemStack(Blocks.FURNACE);
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return BzRecipes.ITEMSTACK_SMELTING_RECIPE.get();
+    }
+
+    public static class ItemStackSmeltingRecipeSerializer implements RecipeSerializer<ItemStackSmeltingRecipe>, BzRecipeSerializer<ItemStackSmeltingRecipe> {
+        private final Codec<ItemStackSmeltingRecipe> codec = RecordCodecBuilder.create(instance -> instance.group(
+                ExtraCodecs.strictOptionalField(Codec.STRING, "group", "").forGetter(abstractCookingRecipe -> abstractCookingRecipe.group),
+                CookingBookCategory.CODEC.fieldOf("category").orElse(CookingBookCategory.MISC).forGetter(abstractCookingRecipe -> abstractCookingRecipe.category),
+                Ingredient.CODEC_NONEMPTY.fieldOf("ingredient").forGetter(abstractCookingRecipe -> abstractCookingRecipe.ingredient),
+                ItemStack.CODEC.fieldOf("result").forGetter(abstractCookingRecipe -> abstractCookingRecipe.result),
+                Codec.FLOAT.fieldOf("experience").orElse(0.0f).forGetter(abstractCookingRecipe -> abstractCookingRecipe.experience),
+                Codec.INT.fieldOf("cookingtime").orElse(200).forGetter(abstractCookingRecipe -> abstractCookingRecipe.cookingTime)
+        ).apply(instance, ItemStackSmeltingRecipe::new));
+
+        @Override
+        public Codec<ItemStackSmeltingRecipe> codec() {
+            return this.codec;
+        }
+
+        @Override
+        public ItemStackSmeltingRecipe fromNetwork(FriendlyByteBuf friendlyByteBuf) {
+            String string = friendlyByteBuf.readUtf();
+            CookingBookCategory cookingBookCategory = friendlyByteBuf.readEnum(CookingBookCategory.class);
+            Ingredient ingredient = Ingredient.fromNetwork(friendlyByteBuf);
+            ItemStack itemStack = friendlyByteBuf.readItem();
+            float f = friendlyByteBuf.readFloat();
+            int i = friendlyByteBuf.readVarInt();
+            return new ItemStackSmeltingRecipe(string, cookingBookCategory, ingredient, itemStack, f, i);
+        }
+
+        @Override
+        public void toNetwork(FriendlyByteBuf friendlyByteBuf, ItemStackSmeltingRecipe itemStackSmeltingRecipe) {
+            friendlyByteBuf.writeUtf(itemStackSmeltingRecipe.group);
+            friendlyByteBuf.writeEnum(itemStackSmeltingRecipe.category());
+            itemStackSmeltingRecipe.ingredient.toNetwork(friendlyByteBuf);
+            friendlyByteBuf.writeItem(itemStackSmeltingRecipe.result);
+            friendlyByteBuf.writeFloat(itemStackSmeltingRecipe.experience);
+            friendlyByteBuf.writeVarInt(itemStackSmeltingRecipe.cookingTime);
+        }
+    }
+}
+
--- common/src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzRecipes.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzRecipes.java
@@ -3,6 +3,7 @@ package com.telepathicgrunt.the_bumblezone.modinit;
 import com.telepathicgrunt.the_bumblezone.Bumblezone;
 import com.telepathicgrunt.the_bumblezone.configs.BzGeneralConfigs;
 import com.telepathicgrunt.the_bumblezone.items.recipes.ContainerCraftingRecipe;
+import com.telepathicgrunt.the_bumblezone.items.recipes.ItemStackSmeltingRecipe;
 import com.telepathicgrunt.the_bumblezone.items.recipes.NbtKeepingShapelessRecipe;
 import com.telepathicgrunt.the_bumblezone.items.recipes.PotionCandleRecipe;
 import com.telepathicgrunt.the_bumblezone.mixin.containers.PotionBrewingAccessor;
@@ -13,6 +14,8 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.SimpleCookingSerializer;
+import net.minecraft.world.item.crafting.SmeltingRecipe;
 
 public class BzRecipes {
     public static final ResourcefulRegistry<RecipeSerializer<?>> RECIPES = ResourcefulRegistries.create(BuiltInRegistries.RECIPE_SERIALIZER, Bumblezone.MODID);
@@ -21,6 +24,7 @@ public class BzRecipes {
     public static final RegistryEntry<RecipeSerializer<ContainerCraftingRecipe>> CONTAINER_CRAFTING_RECIPE = RECIPES.register("container_shapeless_recipe_bz", ContainerCraftingRecipe.Serializer::new);
     public static final RegistryEntry<RecipeSerializer<PotionCandleRecipe>> POTION_CANDLE_RECIPE = RECIPES.register("potion_candle_recipe", PotionCandleRecipe.Serializer::new);
     public static final RegistryEntry<RecipeSerializer<NbtKeepingShapelessRecipe>> NBT_KEEPING_SHAPELESS_RECIPE = RECIPES.register("nbt_keeping_shapeless_recipe", NbtKeepingShapelessRecipe.Serializer::new);
+    public static final RegistryEntry<RecipeSerializer<ItemStackSmeltingRecipe>> ITEMSTACK_SMELTING_RECIPE = RECIPES.register("itemstack_smelting_recipe", ItemStackSmeltingRecipe.ItemStackSmeltingRecipeSerializer::new);
 
     public static void registerBrewingStandRecipes() {
         if (BzGeneralConfigs.glisteringHoneyBrewingRecipe) {
