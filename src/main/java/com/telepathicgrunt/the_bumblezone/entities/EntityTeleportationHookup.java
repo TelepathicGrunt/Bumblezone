b6d9388c8d82700e1294b1aa9b4b51c97b616e91
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-11-29 07:48:11 -0500

    add missing import

--- src/main/java/com/telepathicgrunt/the_bumblezone/entities/EntityTeleportationHookup.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/entities/EntityTeleportationHookup.java
@@ -8,6 +8,7 @@ import com.telepathicgrunt.the_bumblezone.modinit.BzCriterias;
 import com.telepathicgrunt.the_bumblezone.modinit.BzDimension;
 import com.telepathicgrunt.the_bumblezone.modinit.BzTags;
 import com.telepathicgrunt.the_bumblezone.utils.EnchantmentUtils;
+import com.telepathicgrunt.the_bumblezone.utils.GeneralUtils;
 import com.telepathicgrunt.the_bumblezone.world.dimension.BzWorldSavedData;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementProgress;
