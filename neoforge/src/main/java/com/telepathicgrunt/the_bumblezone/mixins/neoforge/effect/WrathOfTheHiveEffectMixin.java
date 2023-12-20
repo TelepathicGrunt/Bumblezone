117c9da266e369416505c5d71a02a5686b2f05fe
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2023-12-20 08:11:30 -0500

    updated athena and fixed mixin

--- neoforge/src/main/java/com/telepathicgrunt/the_bumblezone/mixins/neoforge/effect/WrathOfTheHiveEffectMixin.java
+++ neoforge/src/main/java/com/telepathicgrunt/the_bumblezone/mixins/neoforge/effect/WrathOfTheHiveEffectMixin.java
@@ -1,6 +1,8 @@
 package com.telepathicgrunt.the_bumblezone.mixins.neoforge.effect;
 
 import com.telepathicgrunt.the_bumblezone.effects.WrathOfTheHiveEffect;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.neoforged.neoforge.common.EffectCure;
 import net.neoforged.neoforge.common.EffectCures;
@@ -10,11 +12,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import java.util.Set;
 
 @Mixin(WrathOfTheHiveEffect.class)
-public interface WrathOfTheHiveEffectMixin extends IMobEffectExtension {
+public class WrathOfTheHiveEffectMixin extends MobEffect implements IMobEffectExtension {
+    protected WrathOfTheHiveEffectMixin(MobEffectCategory arg, int i) {
+        super(arg, i);
+    }
 
     @Override
-    default void fillEffectCures(Set<EffectCure> cures, MobEffectInstance effectInstance) {
-        IMobEffectExtension.super.fillEffectCures(cures, effectInstance);
+    public void fillEffectCures(Set<EffectCure> cures, MobEffectInstance effectInstance) {
+        super.fillEffectCures(cures, effectInstance);
         cures.remove(EffectCures.MILK);
     }
 }
