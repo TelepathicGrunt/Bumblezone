0c594c0138ec34d0cf40b39242068165edfc93b8
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-01-01 09:06:52 -0500

    switched to tag for spectrum jetpacks

--- fabric/src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/fabric/SpectrumJetpackCompat.java
+++ fabric/src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/fabric/SpectrumJetpackCompat.java
@@ -5,13 +5,13 @@ import com.telepathicgrunt.the_bumblezone.modcompat.ModCompat;
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.TrinketsApi;
 import net.minecraft.ChatFormatting;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.Tuple;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -20,12 +20,10 @@ import java.util.EnumSet;
 import java.util.List;
 
 public class SpectrumJetpackCompat implements ModCompat {
-    private static Item GEMSTONE_JETPACK;
-    private static Item BEDROCK_JETPACK;
+    private static TagKey<Item> SPECTRUM_JETPACKS;
 
     public SpectrumJetpackCompat() {
-        GEMSTONE_JETPACK = BuiltInRegistries.ITEM.get(new ResourceLocation("spectrumjetpacks", "gemstone_jetpack"));
-        BEDROCK_JETPACK = BuiltInRegistries.ITEM.get(new ResourceLocation("spectrumjetpacks", "bedrock_jetpack"));
+        SPECTRUM_JETPACKS = TagKey.create(Registries.ITEM, new ResourceLocation("spectrumjetpacks", "jetpacks"));
 
        // Keep at end so it is only set to true if no exceptions was thrown during setup
         ModChecker.createJetpackPresent = true;
@@ -38,11 +36,11 @@ public class SpectrumJetpackCompat implements ModCompat {
 
     @Override
     public void restrictFlight(Entity entity, double extraGravity) {
-        if ((GEMSTONE_JETPACK != null || BEDROCK_JETPACK != null) && entity instanceof Player player) {
+        if (SPECTRUM_JETPACKS != null && entity instanceof Player player) {
             TrinketsApi.getTrinketComponent(player).ifPresent(trinketComponent -> {
 
                 List<Tuple<SlotReference, ItemStack>> trinketComponentEquipped = trinketComponent
-                        .getEquipped(itemStack -> itemStack.is(GEMSTONE_JETPACK) || itemStack.is(BEDROCK_JETPACK));
+                        .getEquipped(itemStack -> itemStack.is(SPECTRUM_JETPACKS));
 
                 if (trinketComponentEquipped.size() > 0) {
                     for (Tuple<SlotReference, ItemStack> itemStackTuple : trinketComponentEquipped) {
