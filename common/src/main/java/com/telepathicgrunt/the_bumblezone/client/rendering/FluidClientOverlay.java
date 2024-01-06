ba39646cbb1b508a6fc4e49f761997f9bccdae4a
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-01-05 19:55:55 -0500

    fixed honey fluid brightness overlay

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/client/rendering/FluidClientOverlay.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/client/rendering/FluidClientOverlay.java
@@ -14,6 +14,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.joml.Matrix4f;
 
@@ -52,7 +53,21 @@ public class FluidClientOverlay {
     }
 
     public static float getDimensionBrightnessAtEyes(Entity entity) {
-        float lightLevelAtEyes = entity.level().getRawBrightness(BlockPos.containing(entity.getEyePosition(1)), 0);
+        Level level = entity.level();
+        float lightLevelAtEyes;
+
+        if (level.dimensionType().hasSkyLight()) {
+            lightLevelAtEyes = level.getRawBrightness(BlockPos.containing(entity.getEyePosition(1)), level.getSkyDarken());
+            level.updateSkyBrightness();
+            if (level.isNight()) {
+                float moonBrightness = level.getMoonBrightness();
+                lightLevelAtEyes *= moonBrightness;
+            }
+        }
+        else {
+            lightLevelAtEyes = level.getRawBrightness(BlockPos.containing(entity.getEyePosition(1)), 0);
+        }
+
         return lightLevelAtEyes / 15f;
     }
 }
