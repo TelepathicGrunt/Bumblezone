f85397f7696a752abc27624fe49c36945026e65c
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-01-08 07:27:27 -0500

    safety enchant

--- src/main/java/com/telepathicgrunt/the_bumblezone/enchantments/NeurotoxinsEnchantment.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/enchantments/NeurotoxinsEnchantment.java
@@ -32,7 +32,7 @@ public class NeurotoxinsEnchantment extends Enchantment {
     @Override
     public int getMinCost(int level) {
         if (level > MAX_LEVEL) {
-            return 200;
+            return 201;
         }
 
         return 14 * level;
