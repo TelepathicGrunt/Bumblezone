8b54d762523a573845e2b44ad46b456b00d39025
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-01-13 17:09:24 -0500

    signal getting protection refresh

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,9 @@
 ##### Entities:
 Made Green Sempiternal Sanctum event Rootmin now not knock off equipped armor that has Curse of the Binding or is in `the_bumblezone:essence/life_arena/armor_cannot_be_knocked_off` item tag.
 
+##### Effects:
+Getting Protection of the Hive now spawns a bit of particles at player head to signal the buff has been refreshed.
+
 
 ### **(V.7.3.1 Changes) (1.20.4 Minecraft)**
 
--- common/src/main/java/com/telepathicgrunt/the_bumblezone/blocks/HoneycombBrood.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/blocks/HoneycombBrood.java
@@ -12,6 +12,7 @@ import com.telepathicgrunt.the_bumblezone.modinit.BzCriterias;
 import com.telepathicgrunt.the_bumblezone.modinit.BzEffects;
 import com.telepathicgrunt.the_bumblezone.modinit.BzEntities;
 import com.telepathicgrunt.the_bumblezone.modinit.BzItems;
+import com.telepathicgrunt.the_bumblezone.modinit.BzParticles;
 import com.telepathicgrunt.the_bumblezone.modinit.BzTags;
 import com.telepathicgrunt.the_bumblezone.utils.GeneralUtils;
 import com.telepathicgrunt.the_bumblezone.utils.PlatformHooks;
@@ -253,6 +254,16 @@ public class HoneycombBrood extends ProperFacingBlock {
 
     private static void applyProtection(Player playerEntity, ItemStack itemstack) {
         playerEntity.addEffect(new MobEffectInstance(BzEffects.PROTECTION_OF_THE_HIVE.get(), BzBeeAggressionConfigs.howLongProtectionOfTheHiveLasts, 1, false, false,  true));
+
+        ((ServerLevel)playerEntity.level()).sendParticles(BzParticles.SPARKLE_PARTICLE.get(),
+                playerEntity.position().x(),
+                playerEntity.getEyeY() - 0.25d,
+                playerEntity.position().z(),
+                20,
+                playerEntity.getRandom().nextDouble() / 5 - 0.1d,
+                playerEntity.getRandom().nextDouble() / 5 - 0.1d,
+                playerEntity.getRandom().nextDouble() / 5 - 0.1d,
+                0.1D);
 
         if (playerEntity instanceof ServerPlayer serverPlayer) {
             BzCriterias.GETTING_PROTECTION_TRIGGER.get().trigger(serverPlayer);
