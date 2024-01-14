b38f182ecf6b0480767df25121111e67a0cc7586
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-01-14 09:07:48 -0500

    more spiders

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/configs/BzWorldgenConfigs.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/configs/BzWorldgenConfigs.java
@@ -4,7 +4,7 @@ public class BzWorldgenConfigs {
 
     public static int beeDungeonRarity = 1;
     public static int treeDungeonRarity = 2;
-    public static int spiderInfestedBeeDungeonRarity = 8;
+    public static int spiderInfestedBeeDungeonRarity = 5;
     public static double spawnerRateSpiderBeeDungeon = 0.2D;
 
 }
