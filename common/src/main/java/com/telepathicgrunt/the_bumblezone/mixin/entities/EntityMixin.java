210a681a3f69aecf8951b8fa22629a926ab49f8c
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-01-24 10:03:09 -0500

    Workaround to ensure only on stinger per bee
    https://github.com/TelepathicGrunt/Bumblezone/issues/327

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/mixin/entities/EntityMixin.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/mixin/entities/EntityMixin.java
@@ -6,6 +6,7 @@ import com.telepathicgrunt.the_bumblezone.blocks.FilledPorousHoneycomb;
 import com.telepathicgrunt.the_bumblezone.blocks.PorousHoneycomb;
 import com.telepathicgrunt.the_bumblezone.items.StinglessBeeHelmet;
 import com.telepathicgrunt.the_bumblezone.items.essence.CalmingEssence;
+import com.telepathicgrunt.the_bumblezone.loot.EntityLootDropInterface;
 import com.telepathicgrunt.the_bumblezone.modinit.BzBlocks;
 import com.telepathicgrunt.the_bumblezone.modinit.BzFluids;
 import com.telepathicgrunt.the_bumblezone.modinit.BzTags;
@@ -20,6 +21,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
@@ -27,7 +29,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = Entity.class, priority = 1200)
-public abstract class EntityMixin {
+public abstract class EntityMixin implements EntityLootDropInterface {
+
+    @Unique
+    public boolean thebumblezone_performedEntityDrops = false;
+
+    @Override
+    public boolean thebumblezone_hasPerformedEntityDrops() {
+        return thebumblezone_performedEntityDrops;
+    }
+
+    @Override
+    public void thebumblezone_performedEntityDrops() {
+        thebumblezone_performedEntityDrops = true;
+    }
 
     @Shadow
     public abstract AABB getBoundingBox();
