ad4b6613239f0d1fbd535e0f66af93ce724fa5d1
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-01-31 20:25:44 -0500

    fixed beehive craft mixin
    https://github.com/TelepathicGrunt/Bumblezone/issues/349

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/mixin/entities/ServerPlayerMixin.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/mixin/entities/ServerPlayerMixin.java
@@ -2,12 +2,24 @@ package com.telepathicgrunt.the_bumblezone.mixin.entities;
 
 import com.telepathicgrunt.the_bumblezone.entities.BeeAggression;
 import com.telepathicgrunt.the_bumblezone.entities.mobs.BeeQueenEntity;
+import com.telepathicgrunt.the_bumblezone.modinit.BzCriterias;
+import com.telepathicgrunt.the_bumblezone.modules.PlayerDataHandler;
+import com.telepathicgrunt.the_bumblezone.modules.base.ModuleHelper;
+import com.telepathicgrunt.the_bumblezone.modules.registry.ModuleRegistry;
 import com.telepathicgrunt.the_bumblezone.worldgen.structures.SempiternalSanctumBehavior;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.level.block.BeehiveBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.util.List;
 
 @Mixin(ServerPlayer.class)
 public abstract class ServerPlayerMixin {
@@ -19,5 +31,23 @@ public abstract class ServerPlayerMixin {
         BeeAggression.applyAngerIfInTaggedStructures(serverPlayer);
         BeeQueenEntity.applyMiningFatigueInStructures(serverPlayer);
         SempiternalSanctumBehavior.runStructureMessagesAndFatigue(serverPlayer);
+    }
+
+    @Inject(method = "triggerRecipeCrafted(Lnet/minecraft/world/item/crafting/RecipeHolder;Ljava/util/List;)V",
+            at = @At(value = "HEAD"),
+            locals = LocalCapture.CAPTURE_FAILHARD)
+    private void bumblezone$hookToRecipeCrafting(RecipeHolder<?> recipeHolder, List<ItemStack> list, CallbackInfo ci) {
+        ServerPlayer serverPlayer = (ServerPlayer)(Object)this;
+        ItemStack createdItem = recipeHolder.value().getResultItem(serverPlayer.level().registryAccess());
+
+        if (createdItem.getItem() instanceof BlockItem blockItem &&
+                blockItem.getBlock() instanceof BeehiveBlock &&
+                PlayerDataHandler.rootAdvancementDone(serverPlayer))
+        {
+            ModuleHelper.getModule(serverPlayer, ModuleRegistry.PLAYER_DATA).ifPresent(module -> {
+                module.craftedBeehives++;
+                BzCriterias.BEEHIVE_CRAFTED_TRIGGER.get().trigger(serverPlayer, module.craftedBeehives);
+            });
+        }
     }
 }
