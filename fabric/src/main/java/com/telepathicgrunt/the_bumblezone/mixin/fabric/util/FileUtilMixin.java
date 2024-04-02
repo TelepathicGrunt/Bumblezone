b3bcddb54acbc333986ed1a81fde7a0ccafdf5d3
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-04-02 07:00:12 -0400

    Use Random's improved regex

--- fabric/src/main/java/com/telepathicgrunt/the_bumblezone/mixin/fabric/util/FileUtilMixin.java
+++ fabric/src/main/java/com/telepathicgrunt/the_bumblezone/mixin/fabric/util/FileUtilMixin.java
@@ -10,6 +10,6 @@ public abstract class FileUtilMixin {
 
     @ModifyArg(method = "<clinit>()V", at = @At(value = "INVOKE", target = "Ljava/util/regex/Pattern;compile(Ljava/lang/String;I)Ljava/util/regex/Pattern;"))
     private static String bumblezone$fixMC268617(String regex) {
-        return ".*\\.|(?:COM¹|COM²|COM³|LPT¹|LPT²|LPT³|CLOCK\\$|CON|PRN|AUX|NUL|COM[0-9]|LPT[0-9])(?:\\..*)?";
+        return ".*\\.|(?:CON|PRN|AUX|NUL|CLOCK$|CONIN$|CONOUT$|(?:COM|LPT)[¹²³0-9])(?:\\..*)?";
     }
 }
