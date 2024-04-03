171ef5bb66108f61b1f7bdbcfafb61b2206e920f
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-04-03 19:05:32 -0400

    fixed crystalline flower at world height

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/blocks/CrystallineFlower.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/blocks/CrystallineFlower.java
@@ -349,7 +349,7 @@ public class CrystallineFlower extends BaseEntityBlock {
         int i = 0;
         BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
         BlockState currentState = level.getBlockState(mutable.set(pos.above()));
-        while (currentState.is(BzBlocks.CRYSTALLINE_FLOWER.get()) && mutable.getY() <= level.getMaxBuildHeight()) {
+        while (currentState.is(BzBlocks.CRYSTALLINE_FLOWER.get()) && mutable.getY() < level.getMaxBuildHeight()) {
             i++;
             mutable.move(Direction.UP);
             currentState = level.getBlockState(mutable);
@@ -362,14 +362,20 @@ public class CrystallineFlower extends BaseEntityBlock {
         BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
         mutable.set(pos);
 
-        for(int i = 0; i < scanArea; i++) {
-            BlockState currentState = level.getBlockState(mutable);
-            if (!(currentState.isAir() || currentState.is(BzTags.AIR_LIKE)) && !currentState.is(BzBlocks.CRYSTALLINE_FLOWER.get())) {
+        for (int i = 0; i < scanArea; i++) {
+            if (mutable.getY() >= level.getMaxBuildHeight()) {
                 obstructions.add(true);
             }
             else {
-                obstructions.add(false);
+                BlockState currentState = level.getBlockState(mutable);
+                if (!(currentState.isAir() || currentState.is(BzTags.AIR_LIKE)) && !currentState.is(BzBlocks.CRYSTALLINE_FLOWER.get())) {
+                    obstructions.add(true);
+                }
+                else {
+                    obstructions.add(false);
+                }
             }
+
             mutable.move(Direction.UP);
         }
 
