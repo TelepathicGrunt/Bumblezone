463929f5d088fd9596139751221c64dce91bd854
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-04-03 20:40:00 -0400

    improved mixin

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/mixin/util/FileUtilMixin.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/mixin/util/FileUtilMixin.java
@@ -2,14 +2,21 @@ package com.telepathicgrunt.the_bumblezone.mixin.util;
 
 import net.minecraft.FileUtil;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Mixin(value = FileUtil.class, priority = 700)
 public abstract class FileUtilMixin {
 
-    @ModifyArg(method = "<clinit>()V", at = @At(value = "INVOKE", target = "Ljava/util/regex/Pattern;compile(Ljava/lang/String;I)Ljava/util/regex/Pattern;", ordinal = 1))
-    private static String bumblezone$fixMC268617(String regex) {
-        return ".*\\.|(?:CON|PRN|AUX|NUL|CLOCK\\$|CONIN\\$|CONOUT\\$|(?:COM|LPT)[¹²³0-9])(?:\\..*)?";
+    @Unique
+    private static final Pattern RESERVED_WINDOWS_FILENAMES_BUMBLEZONE = Pattern.compile(".*\\.|(?:CON|PRN|AUX|NUL|CLOCK\\$|CONIN\\$|CONOUT\\$|(?:COM|LPT)[¹²³0-9])(?:\\..*)?", Pattern.CASE_INSENSITIVE);
+
+    @Redirect(method = "isPathPortable(Ljava/nio/file/Path;)Z", at = @At(value = "INVOKE", target = "Ljava/util/regex/Pattern;matcher(Ljava/lang/CharSequence;)Ljava/util/regex/Matcher;", ordinal = 0))
+    private static Matcher bumblezone$fixMC268617(Pattern instance, CharSequence input) {
+        return RESERVED_WINDOWS_FILENAMES_BUMBLEZONE.matcher(input);
     }
 }
