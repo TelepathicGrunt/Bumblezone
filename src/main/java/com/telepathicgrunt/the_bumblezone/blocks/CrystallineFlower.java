7082ef747127c5a8aa8ba1353ec4a692ad27c10a
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-04-13 10:28:22 -0400

    fixed bee stinger with infinity

--- src/main/java/com/telepathicgrunt/the_bumblezone/blocks/CrystallineFlower.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/blocks/CrystallineFlower.java
@@ -21,7 +21,6 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
