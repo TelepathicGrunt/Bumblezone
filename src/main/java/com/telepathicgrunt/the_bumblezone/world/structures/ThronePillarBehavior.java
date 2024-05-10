8fbf91a66a1a07a650f0bf27339e9096d7a3c2ca
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-05-10 11:14:04 -0400

    Added config for bee queen respawning

--- src/main/java/com/telepathicgrunt/the_bumblezone/world/structures/ThronePillarBehavior.java
+++ src/main/java/com/telepathicgrunt/the_bumblezone/world/structures/ThronePillarBehavior.java
@@ -1,5 +1,6 @@
 package com.telepathicgrunt.the_bumblezone.world.structures;
 
+import com.telepathicgrunt.the_bumblezone.configs.BzConfig;
 import com.telepathicgrunt.the_bumblezone.entities.mobs.BeeQueenEntity;
 import com.telepathicgrunt.the_bumblezone.items.EssenceOfTheBees;
 import com.telepathicgrunt.the_bumblezone.items.HoneyCompass;
@@ -41,6 +42,10 @@ public class ThronePillarBehavior {
                         false,
                         false,
                         true));
+            }
+
+            if (!BzConfig.beeQueenRespawning) {
+                return;
             }
 
             List<ItemStack> throneCompasses = new ArrayList<>();
