92e132b4545de0d4367cb323a82bb20937fc1afb
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2024-12-29 10:33:33 -0500

    Big structure optimization

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/utils/BoxOctree.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/utils/BoxOctree.java
@@ -1,6 +1,7 @@
 package com.telepathicgrunt.the_bumblezone.utils;
 
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Vec3i;
 import net.minecraft.world.phys.AABB;
 
@@ -132,6 +133,29 @@ public class BoxOctree {
         else{
             for(AABB innerBox : innerBoxes) {
                 if(innerBox.intersects(axisAlignedBB)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public boolean boundaryContains(BlockPos position) {
+        return boundary.contains(position.getX(), position.getY(), position.getZ());
+    }
+
+    public boolean withinAnyBox(BlockPos position) {
+        if (!childrenOctants.isEmpty()) {
+            for (BoxOctree octree : childrenOctants) {
+                if (octree.boundaryContains(position) && octree.withinAnyBox(position)) {
+                    return true;
+                }
+            }
+        }
+        else {
+            for (AABB innerBox : innerBoxes) {
+                if (innerBox.contains(position.getX(), position.getY(), position.getZ())) {
                     return true;
                 }
             }
