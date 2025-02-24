c7ed984f342d26a0abbf0c9d15bf6a69d727f7a6
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-02-24 07:57:22 -0500

    Fixed fabric builds

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/blocks/blockentities/HoneyCocoonBlockEntity.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/blocks/blockentities/HoneyCocoonBlockEntity.java
@@ -33,7 +33,6 @@ import java.util.stream.IntStream;
 public class HoneyCocoonBlockEntity extends BzRandomizableContainerBlockEntity {
     private NonNullList<ItemStack> itemStacks = NonNullList.withSize(18, ItemStack.EMPTY);
     private UUID blockEntityUuid = null;
-    private boolean lootrInitialInventorySetting = false;
 
     protected HoneyCocoonBlockEntity(BlockEntityType<?> blockEntityType, BlockPos blockPos, BlockState blockState) {
         super(blockEntityType, blockPos, blockState);
@@ -150,10 +149,6 @@ public class HoneyCocoonBlockEntity extends BzRandomizableContainerBlockEntity {
             return false;
         }
 
-        if (this.lootrInitialInventorySetting) {
-            return false;
-        }
-
         BlockState state = this.getBlockState();
         return state.is(BzBlocks.HONEY_COCOON.get()) && state.getValue(HoneyCocoon.IS_LOOT_CONTAINER);
     }
@@ -234,13 +229,5 @@ public class HoneyCocoonBlockEntity extends BzRandomizableContainerBlockEntity {
 
     public long getLootSeed () {
         return this.lootTableSeed;
-    }
-
-    public boolean isLootrInitialInventorySetting() {
-        return lootrInitialInventorySetting;
-    }
-
-    public void setLootrInitialInventorySetting(boolean lootrInitialInventorySetting) {
-        this.lootrInitialInventorySetting = lootrInitialInventorySetting;
     }
 }
