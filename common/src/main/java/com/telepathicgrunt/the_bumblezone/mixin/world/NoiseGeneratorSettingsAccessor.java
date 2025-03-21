08881a28b28b888975d0205311d19ef3c4cffaa7
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-03-21 09:57:25 -0400

    fix noise router accessor

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/mixin/world/NoiseGeneratorSettingsAccessor.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/mixin/world/NoiseGeneratorSettingsAccessor.java
@@ -9,6 +9,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 @Mixin(NoiseGeneratorSettings.class)
 public interface NoiseGeneratorSettingsAccessor {
     @Mutable
-    @Accessor
+    @Accessor("noiseRouter")
     void bumblezone$setNoiseRouter(NoiseRouter noiseRouter);
 }
