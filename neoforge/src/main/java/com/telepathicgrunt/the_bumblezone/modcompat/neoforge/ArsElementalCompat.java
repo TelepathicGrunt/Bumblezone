9473ab324f14767e1e0b36c5022a4bc22392ce71
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-04-28 20:54:27 -0400

    fixed ars elemental check

--- neoforge/src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/neoforge/ArsElementalCompat.java
+++ neoforge/src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/neoforge/ArsElementalCompat.java
@@ -37,11 +37,18 @@ public class ArsElementalCompat implements ModCompat {
 	}
 
 	@SuppressWarnings("ConstantConditions")
-	public static boolean isArsElementalCasting(EffectResolveEvent.Post event) {
-		if (event.spell.getCastMethod() == MethodHomingProjectile.INSTANCE && !BuiltInRegistries.ENTITY_TYPE.get(SPELL_HOMING_PROJ_RL).is(BzTags.TELEPORT_PROJECTILES)) {
+	public static boolean isArsElementalCasting(AbstractCastMethod closestCastMethod) {
+		if (closestCastMethod == MethodHomingProjectile.INSTANCE && !BuiltInRegistries.ENTITY_TYPE.get(SPELL_HOMING_PROJ_RL).get().is(BzTags.TELEPORT_PROJECTILES)) {
 			return true;
 		}
-		else if (event.spell.getCastMethod() == MethodArcProjectile.INSTANCE && !BuiltInRegistries.ENTITY_TYPE.get(SPELL_CURVED_PROJ_RL).is(BzTags.TELEPORT_PROJECTILES)) {
+		else if (closestCastMethod == MethodArcProjectile.INSTANCE && !BuiltInRegistries.ENTITY_TYPE.get(SPELL_CURVED_PROJ_RL).get().is(BzTags.TELEPORT_PROJECTILES)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isArsElementalProjectile(ResourceLocation projectileRL) {
+		if (projectileRL != null && (projectileRL.equals(SPELL_HOMING_PROJ_RL) || projectileRL.equals(SPELL_CURVED_PROJ_RL))) {
 			return true;
 		}
 		return false;
