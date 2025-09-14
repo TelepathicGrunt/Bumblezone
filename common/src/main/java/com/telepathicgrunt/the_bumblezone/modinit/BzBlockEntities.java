e979c5a46826a72d04c101abb5fbb98b29a0a713
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-09-14 10:32:03 -0400

    fixed block entity register

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzBlockEntities.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/modinit/BzBlockEntities.java
@@ -13,15 +13,17 @@ import com.telepathicgrunt.the_bumblezone.blocks.blockentities.StateFocusedBrush
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 
+import java.util.Set;
+
 
 public class BzBlockEntities {
     public static final ResourcefulRegistry<BlockEntityType<?>> BLOCK_ENTITIES = ResourcefulRegistries.create(BuiltInRegistries.BLOCK_ENTITY_TYPE, Bumblezone.MODID);
 
     //Blocks
-    public static final RegistryEntry<BlockEntityType<HoneyCocoonBlockEntity>> HONEY_COCOON = BLOCK_ENTITIES.register("honey_cocoon", () -> BlockEntityType.Builder.of(HoneyCocoonBlockEntity::new, BzBlocks.HONEY_COCOON.get()).build(null));
-    public static final RegistryEntry<BlockEntityType<PotionCandleBlockEntity>> POTION_CANDLE = BLOCK_ENTITIES.register("potion_candle", () -> BlockEntityType.Builder.of(PotionCandleBlockEntity::new, BzBlocks.POTION_BASE_CANDLE.get()).build(null));
-    public static final RegistryEntry<BlockEntityType<CrystallineFlowerBlockEntity>> CRYSTALLINE_FLOWER = BLOCK_ENTITIES.register("crystalline_flower", () -> BlockEntityType.Builder.of(CrystallineFlowerBlockEntity::new, BzBlocks.CRYSTALLINE_FLOWER.get()).build(null));
-    public static final RegistryEntry<BlockEntityType<EssenceBlockEntity>> ESSENCE_BLOCK = BLOCK_ENTITIES.register("essence_block", () -> BlockEntityType.Builder.of(EssenceBlockEntity::new, BzBlocks.ESSENCE_BLOCK_RED.get(), BzBlocks.ESSENCE_BLOCK_PURPLE.get(), BzBlocks.ESSENCE_BLOCK_BLUE.get(), BzBlocks.ESSENCE_BLOCK_GREEN.get(), BzBlocks.ESSENCE_BLOCK_YELLOW.get(), BzBlocks.ESSENCE_BLOCK_WHITE.get()).build(null));
-    public static final RegistryEntry<BlockEntityType<InfinityBarrierBlockEntity>> INFINITY_BARRIER = BLOCK_ENTITIES.register("infinity_barrier", () -> BlockEntityType.Builder.of(InfinityBarrierBlockEntity::new, BzBlocks.INFINITY_BARRIER.get()).build(null));
-    public static final RegistryEntry<BlockEntityType<StateFocusedBrushableBlockEntity>> STATE_FOCUSED_BRUSHABLE_BLOCK_ENTITY = BLOCK_ENTITIES.register("state_focused_brushable_block_entity", () -> BlockEntityType.Builder.of(StateFocusedBrushableBlockEntity::new, BzBlocks.PILE_OF_POLLEN_SUSPICIOUS.get()).build(null));
+    public static final RegistryEntry<BlockEntityType<HoneyCocoonBlockEntity>> HONEY_COCOON = BLOCK_ENTITIES.register("honey_cocoon", () -> new BlockEntityType<>(HoneyCocoonBlockEntity::new, Set.of(BzBlocks.HONEY_COCOON.get())));
+    public static final RegistryEntry<BlockEntityType<PotionCandleBlockEntity>> POTION_CANDLE = BLOCK_ENTITIES.register("potion_candle", () -> new BlockEntityType<>(PotionCandleBlockEntity::new, Set.of(BzBlocks.POTION_BASE_CANDLE.get())));
+    public static final RegistryEntry<BlockEntityType<CrystallineFlowerBlockEntity>> CRYSTALLINE_FLOWER = BLOCK_ENTITIES.register("crystalline_flower", () -> new BlockEntityType<>(CrystallineFlowerBlockEntity::new, Set.of(BzBlocks.CRYSTALLINE_FLOWER.get())));
+    public static final RegistryEntry<BlockEntityType<EssenceBlockEntity>> ESSENCE_BLOCK = BLOCK_ENTITIES.register("essence_block", () -> new BlockEntityType<>(EssenceBlockEntity::new, Set.of(BzBlocks.ESSENCE_BLOCK_RED.get(), BzBlocks.ESSENCE_BLOCK_PURPLE.get(), BzBlocks.ESSENCE_BLOCK_BLUE.get(), BzBlocks.ESSENCE_BLOCK_GREEN.get(), BzBlocks.ESSENCE_BLOCK_YELLOW.get(), BzBlocks.ESSENCE_BLOCK_WHITE.get())));
+    public static final RegistryEntry<BlockEntityType<InfinityBarrierBlockEntity>> INFINITY_BARRIER = BLOCK_ENTITIES.register("infinity_barrier", () -> new BlockEntityType<>(InfinityBarrierBlockEntity::new, Set.of(BzBlocks.INFINITY_BARRIER.get())));
+    public static final RegistryEntry<BlockEntityType<StateFocusedBrushableBlockEntity>> STATE_FOCUSED_BRUSHABLE_BLOCK_ENTITY = BLOCK_ENTITIES.register("state_focused_brushable_block_entity", () -> new BlockEntityType<>(StateFocusedBrushableBlockEntity::new, Set.of(BzBlocks.PILE_OF_POLLEN_SUSPICIOUS.get())));
 }
