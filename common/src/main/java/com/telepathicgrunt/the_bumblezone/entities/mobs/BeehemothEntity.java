caed4f3baf0b334c638f89f8353e1438ffe05d43
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2025-11-28 15:06:53 -0500

    beehemothSpeed synced value not used by client

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/entities/mobs/BeehemothEntity.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/entities/mobs/BeehemothEntity.java
@@ -731,11 +731,11 @@ public class BeehemothEntity extends TamableAnimal implements FlyingAnimal, Sadd
                 double speedModifier = (queenModifier + friendlyModifier) * 0.1D;
 
                 double verticalSpeed = (livingEntity.getLookAngle().y() * Math.abs(livingEntity.zza) * 5);
-                double forwardSpeed = (livingEntity.zza * 10) ;
+                double forwardSpeed = (livingEntity.zza * 10);
                 double strafeSpeed = 0;
 
                 if (livingEntity.zza != 0 || this.movingStraightUp || this.movingStraightDown) {
-                    currentSpeed = Math.min(BzGeneralConfigs.beehemothSpeed * speedModifier, currentSpeed + 0.003D);
+                    currentSpeed = Math.min(BeehemothEntity.beehemothSpeedConfigValue * speedModifier, currentSpeed + 0.003D);
                 }
                 else {
                     currentSpeed = Math.max(0, currentSpeed - 0.2D);
