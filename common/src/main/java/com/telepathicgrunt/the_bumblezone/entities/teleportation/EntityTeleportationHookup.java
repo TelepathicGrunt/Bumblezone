5dbd5d547661631fd68816061ddda6be537e6f4d
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2026-02-12 19:28:57 -0500

    Fixed advancement range check

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/entities/teleportation/EntityTeleportationHookup.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/entities/teleportation/EntityTeleportationHookup.java
@@ -73,10 +73,10 @@ public class EntityTeleportationHookup {
                 return;
             }
 
-            List<PoiRecord> poiInRange = serverLevel.getPoiManager().getInSquare(
+            List<PoiRecord> poiInRange = serverLevel.getPoiManager().getInRange(
                 (pointOfInterestType) -> pointOfInterestType.is(BzTags.IS_NEAR_BEEHIVE_ADVANCEMENT_TRIGGER_POI),
                 serverPlayer.blockPosition(),
-                3,
+                5,
                 PoiManager.Occupancy.ANY
             ).toList();
 
