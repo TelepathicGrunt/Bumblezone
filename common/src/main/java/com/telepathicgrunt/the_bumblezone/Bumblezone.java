a35247460da601acab9cae670b236f7e134d1301
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2026-03-22 22:02:12 -0400

    fixed build error with cherry pick of resourcepack

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/Bumblezone.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/Bumblezone.java
@@ -254,9 +254,9 @@ public class Bumblezone {
         );
 
         event.add(
-                new ResourceLocation(MODID, "shader_emissive"),
+                ResourceLocation.fromNamespaceAndPath(MODID, "shader_emissive"),
                 Component.literal("Bumblezone - Shader Emissive"),
-                AddBuiltinResourcePacks.PackMode.USER_CONTROLLED
+                BzAddBuiltinResourcePacks.PackMode.USER_CONTROLLED
         );
     }
 
