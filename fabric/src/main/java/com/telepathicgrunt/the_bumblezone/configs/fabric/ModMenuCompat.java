56b4a9d6db10a52c9807bb6ba0ef4e82386237ce
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2026-03-27 10:50:59 -0400

    re-enable midnight lib compat

--- fabric/src/main/java/com/telepathicgrunt/the_bumblezone/configs/fabric/ModMenuCompat.java
+++ fabric/src/main/java/com/telepathicgrunt/the_bumblezone/configs/fabric/ModMenuCompat.java
@@ -1,12 +1,14 @@
 package com.telepathicgrunt.the_bumblezone.configs.fabric;
 
+import com.telepathicgrunt.the_bumblezone.Bumblezone;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import eu.midnightdust.lib.config.MidnightConfig;
 
 public class ModMenuCompat implements ModMenuApi {
 
-    // TODO: re-enable when midnight lib updates
-//    @Override
-//    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-//        return parent -> MidnightConfig.getScreen(parent, Bumblezone.MODID);
-//    }
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> MidnightConfig.getScreen(parent, Bumblezone.MODID);
+    }
 }
