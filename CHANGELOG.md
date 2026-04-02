c37e94fae41857093ebee322bd99ea6c50c86e6e
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2026-04-02 12:47:44 -0400

    Show gui bees on bumblezone teleport screen always

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-### **(V.7.10.2 Changes) (1.20.1 Minecraft)*
+### **(V.7.11.0 Changes) (1.20.1 Minecraft)*
 
 #### Entities:
 Added a Bee Queen trade to exchange Banner Patterns for Experience Bottles.
@@ -8,6 +8,7 @@ Added a Bee Queen trade to exchange Banner Patterns for Experience Bottles.
 Added an April Fool's joke for future years, where bees can appear on any in-world gui.
  This is controlled by three configs: showBeesOnGuiAllYearRound, showBeesOnGuiOnAprilFools, and maximumBeesOnGui
  You can activate these gui bees 24/7 with the year round config!
+ The Bumblezone Teleporting screen will have these gui bees always as a small thing if stuck waiting for dimension to load.
 
 
 ### **(V.7.10.1 Changes) (1.20.1 Minecraft)*
--- common/src/main/java/com/telepathicgrunt/the_bumblezone/mixin/client/GameRendererMixin.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/mixin/client/GameRendererMixin.java
@@ -3,7 +3,9 @@ package com.telepathicgrunt.the_bumblezone.mixin.client;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.telepathicgrunt.the_bumblezone.Bumblezone;
 import com.telepathicgrunt.the_bumblezone.client.april_fools.GuiBees;
+import com.telepathicgrunt.the_bumblezone.client.utils.GeneralUtilsClient;
 import com.telepathicgrunt.the_bumblezone.configs.BzClientConfigs;
+import com.telepathicgrunt.the_bumblezone.modinit.BzDimension;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,6 +23,10 @@ public abstract class GameRendererMixin {
         GuiBees.initDateCheck();
         // If player turns off both configs in-game while gui bees are present, turn off right away.
         if (GuiBees.showGuiBeesToday && (BzClientConfigs.showBeesOnGuiOnAprilFools || BzClientConfigs.showBeesOnGuiAllYearRound)) {
+            GuiBees.guiClosed(((GameRenderer)(Object)this).getMinecraft().screen);
+        }
+        // For setting up for Bumblezone's teleporting screen.
+        else if (GeneralUtilsClient.getClientPlayer() != null && GeneralUtilsClient.getClientPlayer().level().dimension() == BzDimension.BZ_WORLD_KEY) {
             GuiBees.guiClosed(((GameRenderer)(Object)this).getMinecraft().screen);
         }
     }
@@ -41,6 +47,9 @@ public abstract class GameRendererMixin {
     {
         // If player turns off both configs in-game while gui bees are present, turn off right away.
         if (GuiBees.showGuiBeesToday && (BzClientConfigs.showBeesOnGuiOnAprilFools || BzClientConfigs.showBeesOnGuiAllYearRound)) {
+            GuiBees.renderBees(((GameRenderer)(Object)this).getMinecraft().screen, guiGraphics, mouseX, mouseY, deltaPastLastTick);
+        }
+        else if (GeneralUtilsClient.getClientPlayer() != null && GeneralUtilsClient.getClientPlayer().level().dimension() == BzDimension.BZ_WORLD_KEY) {
             GuiBees.renderBees(((GameRenderer)(Object)this).getMinecraft().screen, guiGraphics, mouseX, mouseY, deltaPastLastTick);
         }
     }
