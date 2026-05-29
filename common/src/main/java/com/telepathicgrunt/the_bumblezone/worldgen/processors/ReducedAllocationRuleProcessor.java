f459f4418fc481aff5859bcc314992e66c03756b
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2026-05-28 21:42:01 -0400

    Bad javax

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/worldgen/processors/ReducedAllocationRuleProcessor.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/worldgen/processors/ReducedAllocationRuleProcessor.java
@@ -1,29 +1,18 @@
 package com.telepathicgrunt.the_bumblezone.worldgen.processors;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.telepathicgrunt.the_bumblezone.modinit.BzProcessors;
-import com.telepathicgrunt.the_bumblezone.utils.GeneralUtils;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.util.Mth;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.templatesystem.ProcessorRule;
-import net.minecraft.world.level.levelgen.structure.templatesystem.RuleProcessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureProcessorType;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 
-import javax.annotation.Nullable;
-import java.util.HashSet;
 import java.util.List;
 
 public class ReducedAllocationRuleProcessor extends StructureProcessor {
@@ -37,7 +26,6 @@ public class ReducedAllocationRuleProcessor extends StructureProcessor {
     }
 
     // Micro-optimized to reduce object allocation
-    @Nullable
     @Override
     public StructureTemplate.StructureBlockInfo processBlock(
             LevelReader level,
