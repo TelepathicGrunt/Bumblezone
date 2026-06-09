1171f4fbd0b3f6aa910f2028e37acd0168de6e5c
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2026-06-08 21:41:33 -0400

    Pollinated field edge fuzzing

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/worldgen/features/configs/BiomeBasedLayerConfig.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/worldgen/features/configs/BiomeBasedLayerConfig.java
@@ -4,6 +4,9 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.Holder;
 import net.minecraft.resources.Identifier;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.dimension.DimensionType;
@@ -18,7 +21,8 @@ public class BiomeBasedLayerConfig implements FeatureConfiguration {
             BlockState.CODEC.optionalFieldOf("rare_state").forGetter((config) -> config.rareState),
             Codec.floatRange(0, 1).fieldOf("rare_state_chance").orElse(0F).forGetter((config) -> config.rareStateChance),
             Identifier.CODEC.optionalFieldOf("suspicious_block_loot").forGetter((config) -> config.suspiciousBlockLoot),
-            Biome.CODEC.fieldOf("biome").forGetter((config) -> config.biome)
+            Biome.CODEC.fieldOf("biome").forGetter((config) -> config.biome),
+            TagKey.codec(Registries.BIOME).fieldOf("biome_tag_to_not_fuzz_into").forGetter((config) -> config.biomeTagToNotFuzzInto)
         ).apply(instance, BiomeBasedLayerConfig::new));
 
     public final int height;
@@ -27,6 +31,7 @@ public class BiomeBasedLayerConfig implements FeatureConfiguration {
     public final float rareStateChance;
     public final Optional<Identifier> suspiciousBlockLoot;
     public final Holder<Biome> biome;
+    public final TagKey<Biome> biomeTagToNotFuzzInto;
 
     public BiomeBasedLayerConfig(
             int height,
@@ -34,7 +39,8 @@ public class BiomeBasedLayerConfig implements FeatureConfiguration {
             Optional<BlockState> rareState,
             float rareStateChance,
             Optional<Identifier> suspiciousBlockLoot,
-            Holder<Biome> biome)
+            Holder<Biome> biome,
+            TagKey<Biome> biomeTagToNotFuzzInto)
     {
         this.height = height;
         this.state = state;
@@ -42,5 +48,6 @@ public class BiomeBasedLayerConfig implements FeatureConfiguration {
         this.rareStateChance = rareStateChance;
         this.suspiciousBlockLoot = suspiciousBlockLoot;
         this.biome = biome;
+        this.biomeTagToNotFuzzInto = biomeTagToNotFuzzInto;
     }
 }
