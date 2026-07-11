a848db6e79f0b392e2d691cfe414fd7768473ef1
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2026-07-11 12:46:25 -0400

    Fixed more compile issues

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/client/rendering/essence/EssenceOverlay.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/client/rendering/essence/EssenceOverlay.java
@@ -1,18 +1,19 @@
 package com.telepathicgrunt.the_bumblezone.client.rendering.essence;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.telepathicgrunt.the_bumblezone.Bumblezone;
 import com.telepathicgrunt.the_bumblezone.configs.BzClientConfigs;
 import com.telepathicgrunt.the_bumblezone.items.essence.AbilityEssenceItem;
 import com.telepathicgrunt.the_bumblezone.modinit.BzDataComponents;
 import com.telepathicgrunt.the_bumblezone.utils.GeneralUtils;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.resources.Identifier;
+import net.minecraft.util.ARGB;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import org.joml.Matrix4f;
+import org.joml.Matrix3x2f;
+import org.joml.Matrix3x2fStack;
 
 public class EssenceOverlay {
     private static final Identifier TEXTURE_OVERLAY_1 = Identifier.fromNamespaceAndPath(Bumblezone.MODID, "textures/misc/active_essence_overlay.png");
@@ -24,17 +25,13 @@ public class EssenceOverlay {
 
         ItemStack offhandItem = player.getOffhandItem();
 
-        if (!(offhandItem.getItem() instanceof AbilityEssenceItem abilityEssenceItem) || player.getCooldowns().isOnCooldown(abilityEssenceItem)) {
+        if (!(offhandItem.getItem() instanceof AbilityEssenceItem abilityEssenceItem) || player.getCooldowns().isOnCooldown(offhandItem)) {
             return;
         }
 
         if (!offhandItem.get(BzDataComponents.ABILITY_ESSENCE_ACTIVITY_DATA.get()).isActive()) {
             return;
         }
-
-        RenderSystem.disableDepthTest();
-        RenderSystem.depthMask(false);
-        RenderSystem.enableBlend();
 
         float red = GeneralUtils.getRed(abilityEssenceItem.getColor()) / 256f;
         float green = GeneralUtils.getGreen(abilityEssenceItem.getColor()) / 256f;
@@ -43,15 +40,15 @@ public class EssenceOverlay {
         int remainingUse = abilityEssenceItem.getAbilityUseRemaining(offhandItem);
         float percentageLeft = (float)remainingUse / abilityEssenceItem.getMaxAbilityUseAmount();
         float opacity = 0.1f + (percentageLeft * 0.2f);
-        guiGraphics.setColor(red, green, blue, opacity);
+        int color = ARGB.colorFromFloat(opacity, red, green, blue);
 
-        PoseStack poseStack = guiGraphics.pose();
+        Matrix3x2fStack poseStack = guiGraphics.pose();
 
         float rotationX = guiGraphics.guiWidth() / 2f;
         float rotationY = guiGraphics.guiHeight();
 
         for(int layer = 0; layer < BzClientConfigs.essenceItemHUDVisualEffectLayers; layer++) {
-            poseStack.pushPose();
+            poseStack.pushMatrix();
 
             int rotationDirection = layer % 2 == 1 ? -1 : 1;
             float squash = 0.12f - (layer * 0.04f);
@@ -60,44 +57,39 @@ public class EssenceOverlay {
             double degrees = ((currentMillisecond / spinSlowdown) * BzClientConfigs.essenceItemHUDVisualEffectSpeed) * rotationDirection;
             float angle = (float) ((degrees + 45) * Mth.DEG_TO_RAD);
 
-            Matrix4f rotationMatrix = new Matrix4f(
-                    Mth.cos(angle), -Mth.sin(angle), 0, 0,
-                    Mth.sin(angle), Mth.cos(angle), 0, 0,
-                    0, 0, 1, 0,
-                    0, 0, 0, 1
+            Matrix3x2f rotationMatrix = new Matrix3x2f(
+                    Mth.cos(angle), -Mth.sin(angle),
+                    Mth.sin(angle), Mth.cos(angle),
+                    0, 0
             );
-            Matrix4f scalingMatrix = new Matrix4f(
-                    1, 0, 0, 0,
-                    0, squash, 0, 0,
-                    0, 0, 1, 0,
-                    0, 0, 0, 1
+            Matrix3x2f scalingMatrix = new Matrix3x2f(
+                    1, 0,
+                    0, squash,
+                    0, 0
             );
-            Matrix4f translationMatrix = new Matrix4f(
-                    1, 0, 0, 0,
-                    0, 1, 0, 0,
-                    0, 0, 1, 0,
-                    rotationX, rotationY, 0, 1
+            Matrix3x2f translationMatrix = new Matrix3x2f(
+                    1, 0,
+                    0, 1,
+                    rotationX, rotationY
             );
 
-            poseStack.mulPose(translationMatrix.mul(scalingMatrix).mul(rotationMatrix));
+            poseStack.mul(translationMatrix.mul(scalingMatrix).mul(rotationMatrix));
 
-            guiGraphics.blit(TEXTURE_OVERLAY_1,
+
+            guiGraphics.blit(
+                    RenderPipelines.GUI_TEXTURED,
+                    TEXTURE_OVERLAY_1,
                     (int) -rotationX,
                     (int) -rotationX,
-                    -90,
-                    0.0F,
-                    0.0F,
+                    0,
+                    0,
                     guiGraphics.guiWidth(),
                     guiGraphics.guiWidth(),
                     guiGraphics.guiWidth(),
-                    guiGraphics.guiWidth());
+                    guiGraphics.guiWidth(),
+                    color);
 
-            poseStack.popPose();
+            poseStack.popMatrix();
         }
-
-        RenderSystem.disableBlend();
-        RenderSystem.depthMask(true);
-        RenderSystem.enableDepthTest();
-        guiGraphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
