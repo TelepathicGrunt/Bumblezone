e3cedd400651492c28e0c56de9f7744af613025f
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2026-08-19 22:11:12 -0400

    Teleportation improvement

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/utils/GeneralUtils.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/utils/GeneralUtils.java
@@ -252,6 +252,27 @@ public class GeneralUtils {
 
     //////////////////////////////////////////////
 
+    public static int getFirstSurfaceYFromPos(LevelReader worldView, BlockPos pos) {
+        BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
+        mutable.set(pos);
+        ChunkAccess currentChunk = worldView.getChunk(mutable);
+        BlockState currentState = currentChunk.getBlockState(mutable);
+
+        if (!isReplaceableByStructures(currentState)) {
+            while (mutable.getY() >= worldView.getMinY() && !isReplaceableByStructures(currentState)) {
+                mutable.move(Direction.DOWN);
+                currentState = currentChunk.getBlockState(mutable);
+            }
+        }
+
+        while(mutable.getY() >= worldView.getMinY() && isReplaceableByStructures(currentState)) {
+            mutable.move(Direction.DOWN);
+            currentState = currentChunk.getBlockState(mutable);
+        }
+
+        return mutable.getY();
+    }
+
     public static int getFirstLandYFromPos(LevelReader worldView, BlockPos pos) {
         BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
         mutable.set(pos);
