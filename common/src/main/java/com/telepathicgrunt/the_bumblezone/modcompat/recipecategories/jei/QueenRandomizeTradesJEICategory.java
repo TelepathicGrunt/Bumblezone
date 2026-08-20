e92ed74a3a81efc791808dda90ccb7a5084cf125
TelepathicGrunt <40846040+TelepathicGrunt@users.noreply.github.com>
2026-08-19 21:46:17 -0400

    fix import missing

--- common/src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/recipecategories/jei/QueenRandomizeTradesJEICategory.java
+++ common/src/main/java/com/telepathicgrunt/the_bumblezone/modcompat/recipecategories/jei/QueenRandomizeTradesJEICategory.java
@@ -14,6 +14,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.CommonColors;
 
 import java.util.List;
 
